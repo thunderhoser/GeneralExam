3174deaddf5b7c0e7975d4531065d7f8b1961906
thunderhoser <ryan.lagerquist@ou.edu>
2018-02-16 16:33:33 -0600

    Added front_utils.py.

---
+++ generalexam/ge_utils/front_utils.py
@@ -0,0 +1,301 @@
+"""Methods for handling atmospheric fronts.
+
+A front may be represented as either a polyline or a set of grid points.
+"""
+
+import numpy
+import shapely.geometry
+from scipy.ndimage.morphology import binary_dilation
+from gewittergefahr.gg_utils import nwp_model_utils
+from gewittergefahr.gg_utils import polygons
+from gewittergefahr.gg_utils import error_checking
+
+
+def _check_polyline(vertex_x_coords_metres, vertex_y_coords_metres):
+    """Checks polyline for errors.
+
+    V = number of vertices
+
+    :param vertex_x_coords_metres: length-V numpy array with x-coordinates of
+        vertices.
+    :param vertex_y_coords_metres: length-V numpy array with y-coordinates of
+        vertices.
+    """
+
+    error_checking.assert_is_numpy_array_without_nan(vertex_x_coords_metres)
+    error_checking.assert_is_numpy_array(
+        vertex_x_coords_metres, num_dimensions=1)
+    num_vertices = len(vertex_x_coords_metres)
+
+    error_checking.assert_is_numpy_array_without_nan(vertex_y_coords_metres)
+    error_checking.assert_is_numpy_array(
+        vertex_y_coords_metres, exact_dimensions=numpy.array([num_vertices]))
+
+
+def _vertex_arrays_to_list(vertex_x_coords_metres, vertex_y_coords_metres):
+    """Converts set of vertices from two arrays to one list.
+
+    V = number of vertices
+
+    :param vertex_x_coords_metres: length-V numpy array with x-coordinates of
+        vertices.
+    :param vertex_y_coords_metres: length-V numpy array with y-coordinates of
+        vertices.
+    :return: vertex_list_xy_metres: length-V list, where each element is an
+        (x, y) tuple.
+    """
+
+    _check_polyline(vertex_x_coords_metres=vertex_x_coords_metres,
+                    vertex_y_coords_metres=vertex_y_coords_metres)
+
+    num_vertices = len(vertex_x_coords_metres)
+    vertex_list_xy_metres = []
+    for i in range(num_vertices):
+        vertex_list_xy_metres.append(
+            (vertex_x_coords_metres[i], vertex_y_coords_metres[i]))
+
+    return vertex_list_xy_metres
+
+
+def _polyline_from_vertex_arrays_to_linestring(
+        vertex_x_coords_metres, vertex_y_coords_metres):
+    """Converts polyline from vertex arrays to `shapely.geometry.LineString`.
+
+    V = number of vertices
+
+    :param vertex_x_coords_metres: length-V numpy array with x-coordinates of
+        vertices.
+    :param vertex_y_coords_metres: length-V numpy array with y-coordinates of
+        vertices.
+    :return: linestring_object_xy_metres: Instance of
+        `shapely.geometry.LineString`, with vertex coordinates in metres.
+    :raises: ValueError: if resulting LineString object is invalid.
+    """
+
+    vertex_list_xy_metres = _vertex_arrays_to_list(
+        vertex_x_coords_metres=vertex_x_coords_metres,
+        vertex_y_coords_metres=vertex_y_coords_metres)
+
+    linestring_object_xy_metres = shapely.geometry.LineString(
+        vertex_list_xy_metres)
+    if not linestring_object_xy_metres.is_valid:
+        raise ValueError('Resulting LineString object is invalid.')
+
+    return linestring_object_xy_metres
+
+
+def _grid_cell_to_polygon(
+        grid_point_x_metres, grid_point_y_metres, x_spacing_metres,
+        y_spacing_metres):
+    """Converts a grid cell to a polygon.
+
+    This method assumes that the grid is regular in x-y coordinates, not in lat-
+    long coordinates.
+
+    :param grid_point_x_metres: x-coordinate of center point ("grid point").
+    :param grid_point_y_metres: y-coordinate of center point ("grid point").
+    :param x_spacing_metres: Spacing between adjacent grid points in
+        x-direction.
+    :param y_spacing_metres: Spacing between adjacent grid points in
+        y-direction.
+    :return: grid_cell_edge_polygon_xy_metres: Instance of
+        `shapely.geometry.Polygon`, where each vertex is a corner of the grid
+        cell.  Coordinates are in metres.
+    """
+
+    x_min_metres = grid_point_x_metres - x_spacing_metres / 2
+    x_max_metres = grid_point_x_metres + x_spacing_metres / 2
+    y_min_metres = grid_point_y_metres - y_spacing_metres / 2
+    y_max_metres = grid_point_y_metres + y_spacing_metres / 2
+
+    vertex_x_coords_metres = numpy.array(
+        [x_min_metres, x_max_metres, x_max_metres, x_min_metres, x_min_metres])
+    vertex_y_coords_metres = numpy.array(
+        [y_min_metres, y_min_metres, y_max_metres, y_max_metres, y_min_metres])
+
+    return polygons.vertex_arrays_to_polygon_object(
+        exterior_x_coords=vertex_x_coords_metres,
+        exterior_y_coords=vertex_y_coords_metres)
+
+
+def _polyline_to_grid_points(
+        polyline_x_coords_metres, polyline_y_coords_metres,
+        grid_point_x_coords_metres, grid_point_y_coords_metres):
+    """Converts a polyline to a set of grid points.
+
+    P = number of vertices in polyline
+    M = number of grid rows (unique grid-point y-coordinates)
+    N = number of grid columns (unique grid-point x-coordinates)
+    Q = number of grid points in polyline
+
+    This method assumes that `grid_point_x_coords_metres` and
+    `grid_point_y_coords_metres` are sorted in ascending order and equally
+    spaced.  In other words, the grid must be *regular* in x-y.
+
+    :param polyline_x_coords_metres: length-P numpy array with x-coordinates in
+        polyline.
+    :param polyline_y_coords_metres: length-P numpy array with y-coordinates in
+        polyline.
+    :param grid_point_x_coords_metres: length-N numpy array with unique
+        x-coordinates of grid points.
+    :param grid_point_y_coords_metres: length-M numpy array with unique
+        y-coordinates of grid points.
+    :return: rows_in_polyline: length-Q numpy array with row indices (integers)
+        of grid points in polyline.
+    :return: columns_in_polyline: length-Q numpy array with column indices
+        (integers) of grid points in polyline.
+    """
+
+    polyline_object_xy_metres = _polyline_from_vertex_arrays_to_linestring(
+        vertex_x_coords_metres=polyline_x_coords_metres,
+        vertex_y_coords_metres=polyline_y_coords_metres)
+
+    x_spacing_metres = (
+        grid_point_x_coords_metres[1] - grid_point_x_coords_metres[0])
+    y_spacing_metres = (
+        grid_point_y_coords_metres[1] - grid_point_y_coords_metres[0])
+
+    x_min_to_consider_metres = numpy.min(
+        polyline_x_coords_metres) - x_spacing_metres
+    x_max_to_consider_metres = numpy.max(
+        polyline_x_coords_metres) + x_spacing_metres
+    y_min_to_consider_metres = numpy.min(
+        polyline_y_coords_metres) - y_spacing_metres
+    y_max_to_consider_metres = numpy.max(
+        polyline_y_coords_metres) + y_spacing_metres
+
+    x_in_range_indices = numpy.where(numpy.logical_and(
+        grid_point_x_coords_metres >= x_min_to_consider_metres,
+        grid_point_x_coords_metres <= x_max_to_consider_metres))[0]
+    y_in_range_indices = numpy.where(numpy.logical_and(
+        grid_point_y_coords_metres >= y_min_to_consider_metres,
+        grid_point_y_coords_metres <= y_max_to_consider_metres))[0]
+
+    row_offset = numpy.min(y_in_range_indices)
+    column_offset = numpy.min(x_in_range_indices)
+
+    grid_points_x_to_consider_metres = grid_point_x_coords_metres[
+        x_in_range_indices]
+    grid_points_y_to_consider_metres = grid_point_y_coords_metres[
+        y_in_range_indices]
+
+    rows_in_polyline = []
+    columns_in_polyline = []
+    num_rows_to_consider = len(grid_points_y_to_consider_metres)
+    num_columns_to_consider = len(grid_points_x_to_consider_metres)
+
+    for i in range(num_rows_to_consider):
+        for j in range(num_columns_to_consider):
+            this_grid_cell_edge_polygon_xy_metres = _grid_cell_to_polygon(
+                grid_point_x_metres=grid_points_x_to_consider_metres[j],
+                grid_point_y_metres=grid_points_y_to_consider_metres[i],
+                x_spacing_metres=x_spacing_metres,
+                y_spacing_metres=y_spacing_metres)
+
+            this_intersection_flag = (
+                this_grid_cell_edge_polygon_xy_metres.intersects(
+                    polyline_object_xy_metres) or
+                this_grid_cell_edge_polygon_xy_metres.touches(
+                    polyline_object_xy_metres))
+            if not this_intersection_flag:
+                continue
+
+            rows_in_polyline.append(i + row_offset)
+            columns_in_polyline.append(j + column_offset)
+
+    return numpy.array(rows_in_polyline), numpy.array(columns_in_polyline)
+
+
+def _grid_points_to_binary_image(
+        rows_in_object, columns_in_object, num_grid_rows, num_grid_columns):
+    """Converts set of grid points in object to a binary image matrix.
+
+    P = number of points in object
+    M = number of grid rows (unique grid-point y-coordinates)
+    N = number of grid columns (unique grid-point x-coordinates)
+
+    :param rows_in_object: length-P numpy array with indices (integers) of rows
+        in object.
+    :param columns_in_object: length-P numpy array with indices (integers) of
+        columns in object.
+    :param num_grid_rows: Number of rows in grid.
+    :param num_grid_columns: Number of columns in grid.
+    :return: binary_matrix: M-by-N numpy array of Boolean flags.  If
+        binary_matrix[i, j] = True, grid cell [i, j] is part of the object.
+        Otherwise, grid cell [i, j] is *not* part of the object.
+    """
+
+    binary_matrix = numpy.full(
+        (num_grid_rows, num_grid_columns), False, dtype=bool)
+    binary_matrix[rows_in_object, columns_in_object] = True
+    return binary_matrix
+
+
+def _dilate_binary_image(binary_matrix, dilation_half_width_in_grid_cells):
+    """Dilates a binary image matrix.
+
+    M = number of grid rows (unique grid-point y-coordinates)
+    N = number of grid columns (unique grid-point x-coordinates)
+
+    :param binary_matrix: M-by-N numpy array of Boolean flags.
+    :param dilation_half_width_in_grid_cells: Half-width of dilation window.
+    :return: binary_matrix: Same as input, except dilated.
+    """
+
+    return binary_dilation(
+        binary_matrix, iterations=dilation_half_width_in_grid_cells, origin=0,
+        border_value=0)
+
+
+def polyline_to_binary_narr_grid(
+        polyline_latitudes_deg, polyline_longitudes_deg,
+        dilation_half_width_in_grid_cells=1):
+    """Converts polyline to binary image with dimensions of NARR* grid.
+
+    * NARR = North American Regional Reanalysis
+
+    P = number of vertices in polyline
+    M = number of rows in NARR grid = 277
+    N = number of columns in NARR grid = 349
+
+    This method assumes that `grid_point_x_coords_metres` and
+    `grid_point_y_coords_metres` are sorted in ascending order and equally
+    spaced.  In other words, the grid must be *regular* in x-y.
+
+    :param polyline_latitudes_deg: length-P numpy array with latitudes (deg N)
+        in polyline.
+    :param polyline_longitudes_deg: length-P numpy array with longitudes (deg E)
+        in polyline.
+    :param dilation_half_width_in_grid_cells: Half-width of dilation window for
+        `_dilate_binary_image`.
+    :return: binary_matrix: M-by-N numpy array of Boolean flags.  If
+        binary_matrix[i, j] = True, the polyline passes through grid cell
+        [i, j].  Otherwise, the polyline does not pass through grid cell [i, j].
+    """
+
+    polyline_x_coords_metres, polyline_y_coords_metres = (
+        nwp_model_utils.project_latlng_to_xy(
+            latitudes_deg=polyline_latitudes_deg,
+            longitudes_deg=polyline_longitudes_deg,
+            model_name=nwp_model_utils.NARR_MODEL_NAME))
+
+    grid_point_x_coords_metres, grid_point_y_coords_metres = (
+        nwp_model_utils.get_xy_grid_points(
+            model_name=nwp_model_utils.NARR_MODEL_NAME))
+
+    rows_in_polyline, columns_in_polyline = _polyline_to_grid_points(
+        polyline_x_coords_metres=polyline_x_coords_metres,
+        polyline_y_coords_metres=polyline_y_coords_metres,
+        grid_point_x_coords_metres=grid_point_x_coords_metres,
+        grid_point_y_coords_metres=grid_point_y_coords_metres)
+
+    num_grid_rows, num_grid_columns = nwp_model_utils.get_grid_dimensions(
+        model_name=nwp_model_utils.NARR_MODEL_NAME)
+
+    binary_matrix = _grid_points_to_binary_image(
+        rows_in_object=rows_in_polyline, columns_in_object=columns_in_polyline,
+        num_grid_rows=num_grid_rows, num_grid_columns=num_grid_columns)
+
+    return _dilate_binary_image(
+        binary_matrix=binary_matrix,
+        dilation_half_width_in_grid_cells=dilation_half_width_in_grid_cells)
---
+++ generalexam/ge_utils/front_utils_test.py
@@ -0,0 +1,128 @@
+"""Unit tests for front_utils.py."""
+
+import copy
+import unittest
+import numpy
+from generalexam.ge_utils import front_utils
+
+TOLERANCE = 1e-6
+
+GRID_SPACING_X_METRES = 1.
+GRID_SPACING_Y_METRES = 1.
+
+ONE_GRID_POINT_X_METRES = 1.
+ONE_GRID_POINT_Y_METRES = 2.
+ONE_GRID_VERTEX_COORDS_X_METRES = numpy.array([0.5, 1.5, 1.5, 0.5, 0.5])
+ONE_GRID_VERTEX_COORDS_Y_METRES = numpy.array([1.5, 1.5, 2.5, 2.5, 1.5])
+
+POLYLINE_X_COORDS_METRES = numpy.array(
+    [3.3, 2.3, 1.8, 1.5, 1.3, 1.4, 1.7, 2.1, 2.6, 2.8])
+POLYLINE_Y_COORDS_METRES = numpy.array(
+    [5.5, 4.7, 4.1, 3.5, 2.5, 1.5, 0.9, 0.1, -0.2, -0.5])
+POLYLINE_VERTEX_LIST_XY_METRES = [
+    (3.3, 5.5), (2.3, 4.7), (1.8, 4.1), (1.5, 3.5), (1.3, 2.5),
+    (1.4, 1.5), (1.7, 0.9), (2.1, 0.1), (2.6, -0.2), (2.8, -0.5)]
+
+NUM_GRID_ROWS = 6
+NUM_GRID_COLUMNS = 8
+GRID_POINT_X_COORDS_METRES = numpy.array([0., 1., 2., 3., 4., 5., 6., 7.])
+GRID_POINT_Y_COORDS_METRES = numpy.array([0., 1., 2., 3., 4., 5.])
+
+GRID_ROWS_IN_POLYLINE = numpy.array(
+    [0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5], dtype=int)
+GRID_COLUMNS_IN_POLYLINE = numpy.array(
+    [2, 3, 1, 2, 1, 1, 2, 1, 2, 2, 3], dtype=int)
+
+POLYLINE_AS_BINARY_MATRIX = numpy.array([[0, 0, 1, 1, 0, 0, 0, 0],
+                                         [0, 1, 1, 0, 0, 0, 0, 0],
+                                         [0, 1, 0, 0, 0, 0, 0, 0],
+                                         [0, 1, 1, 0, 0, 0, 0, 0],
+                                         [0, 1, 1, 0, 0, 0, 0, 0],
+                                         [0, 0, 1, 1, 0, 0, 0, 0]])
+
+DILATION_HALF_WIDTH_IN_GRID_CELLS = 1
+DILATED_BINARY_MATRIX = numpy.array([[0, 1, 1, 1, 1, 0, 0, 0],
+                                     [1, 1, 1, 1, 0, 0, 0, 0],
+                                     [1, 1, 1, 0, 0, 0, 0, 0],
+                                     [1, 1, 1, 1, 0, 0, 0, 0],
+                                     [1, 1, 1, 1, 0, 0, 0, 0],
+                                     [0, 1, 1, 1, 1, 0, 0, 0]])
+
+
+class FrontUtilsTests(unittest.TestCase):
+    """Each method is a unit test for front_utils.py."""
+
+    def test_vertex_arrays_to_list(self):
+        """Ensures correct output from _vertex_arrays_to_list."""
+
+        this_vertex_list_xy_metres = front_utils._vertex_arrays_to_list(
+            vertex_x_coords_metres=POLYLINE_X_COORDS_METRES,
+            vertex_y_coords_metres=POLYLINE_Y_COORDS_METRES)
+
+        self.assertTrue(len(this_vertex_list_xy_metres) ==
+                        len(POLYLINE_VERTEX_LIST_XY_METRES))
+
+        for i in range(len(this_vertex_list_xy_metres)):
+            self.assertTrue(numpy.allclose(
+                this_vertex_list_xy_metres[i],
+                POLYLINE_VERTEX_LIST_XY_METRES[i], atol=TOLERANCE))
+
+    def test_grid_cell_to_polygon(self):
+        """Ensures correct output from _grid_cell_to_polygon."""
+
+        this_polygon_object_xy_metres = front_utils._grid_cell_to_polygon(
+            grid_point_x_metres=ONE_GRID_POINT_X_METRES,
+            grid_point_y_metres=ONE_GRID_POINT_Y_METRES,
+            x_spacing_metres=GRID_SPACING_X_METRES,
+            y_spacing_metres=GRID_SPACING_Y_METRES)
+
+        these_vertex_x_metres = numpy.array(
+            this_polygon_object_xy_metres.exterior.xy[0])
+        these_vertex_y_metres = numpy.array(
+            this_polygon_object_xy_metres.exterior.xy[1])
+
+        self.assertTrue(numpy.allclose(
+            these_vertex_x_metres, ONE_GRID_VERTEX_COORDS_X_METRES,
+            atol=TOLERANCE))
+        self.assertTrue(numpy.allclose(
+            these_vertex_y_metres, ONE_GRID_VERTEX_COORDS_Y_METRES,
+            atol=TOLERANCE))
+
+    def test_polyline_to_grid_points(self):
+        """Ensures correct output from _polyline_to_grid_points."""
+
+        these_rows, these_columns = front_utils._polyline_to_grid_points(
+            polyline_x_coords_metres=POLYLINE_X_COORDS_METRES,
+            polyline_y_coords_metres=POLYLINE_Y_COORDS_METRES,
+            grid_point_x_coords_metres=GRID_POINT_X_COORDS_METRES,
+            grid_point_y_coords_metres=GRID_POINT_Y_COORDS_METRES)
+
+        self.assertTrue(numpy.array_equal(these_rows, GRID_ROWS_IN_POLYLINE))
+        self.assertTrue(numpy.array_equal(
+            these_columns, GRID_COLUMNS_IN_POLYLINE))
+
+    def test_grid_points_to_binary_image(self):
+        """Ensures correct output from _grid_points_to_binary_image."""
+
+        this_binary_matrix = front_utils._grid_points_to_binary_image(
+            rows_in_object=GRID_ROWS_IN_POLYLINE,
+            columns_in_object=GRID_COLUMNS_IN_POLYLINE,
+            num_grid_rows=NUM_GRID_ROWS, num_grid_columns=NUM_GRID_COLUMNS)
+
+        self.assertTrue(numpy.array_equal(
+            this_binary_matrix, POLYLINE_AS_BINARY_MATRIX))
+
+    def test_dilate_binary_image(self):
+        """Ensures correct output from _dilate_binary_image."""
+
+        this_input_matrix = copy.deepcopy(POLYLINE_AS_BINARY_MATRIX)
+        this_dilated_matrix = front_utils._dilate_binary_image(
+            binary_matrix=this_input_matrix,
+            dilation_half_width_in_grid_cells=DILATION_HALF_WIDTH_IN_GRID_CELLS)
+
+        self.assertTrue(numpy.array_equal(
+            this_dilated_matrix, DILATED_BINARY_MATRIX))
+
+
+if __name__ == '__main__':
+    unittest.main()
