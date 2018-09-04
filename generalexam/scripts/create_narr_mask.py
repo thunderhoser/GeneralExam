c78e9c55736846d55a53e05ef929ac5c4d147287
thunderhoser <ryan.lagerquist@ou.edu>
2018-09-04 02:02:36 -0500

    Finalized create_narr_mask.py.

--- generalexam/scripts/create_narr_mask.py
+++ generalexam/scripts/create_narr_mask.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as pyplot
 from gewittergefahr.gg_utils import time_conversion
 from gewittergefahr.gg_utils import time_periods
 from gewittergefahr.gg_utils import nwp_model_utils
+from gewittergefahr.gg_utils import error_checking
 from gewittergefahr.gg_utils import file_system_utils
 from gewittergefahr.plotting import plotting_utils
 from generalexam.ge_io import fronts_io
@@ -37,6 +38,7 @@ FRONTAL_GRID_DIR_ARG_NAME = 'input_frontal_grid_dir_name'
 FIRST_TIME_ARG_NAME = 'first_time_string'
 LAST_TIME_ARG_NAME = 'last_time_string'
 DILATION_DISTANCE_ARG_NAME = 'dilation_distance_metres'
+MIN_FRONTAL_GRID_CELLS_ARG_NAME = 'min_frontal_grid_cells'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 FRONTAL_GRID_DIR_HELP_STRING = (
@@ -54,12 +56,18 @@ DILATION_DISTANCE_HELP_STRING = (
     'At each time step, both warm and cold fronts will be dilated with this '
     'distance buffer.')
 
+MIN_FRONTAL_GRID_CELLS_HELP_STRING = (
+    'Masking threshold.  Any grid cell with >= `{0:s}` fronts will be retained.'
+    '  All other grid cells will be masked out.'
+).format(MIN_FRONTAL_GRID_CELLS_ARG_NAME)
+
 OUTPUT_DIR_HELP_STRING = (
     'Path to output directory.  Results will be saved here.')
 
 DEFAULT_TOP_FRONTAL_GRID_DIR_NAME = (
     '/condo/swatwork/ralager/fronts/narr_grids/no_dilation')
 DEFAULT_DILATION_DISTANCE_METRES = 50000.
+DEFAULT_MIN_FRONTAL_GRID_CELLS = 100
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
@@ -79,11 +87,17 @@ INPUT_ARG_PARSER.add_argument(
     help=DILATION_DISTANCE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
+    '--' + MIN_FRONTAL_GRID_CELLS_ARG_NAME, type=int, required=False,
+    default=DEFAULT_MIN_FRONTAL_GRID_CELLS,
+    help=MIN_FRONTAL_GRID_CELLS_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
     help=OUTPUT_DIR_HELP_STRING)
 
 
-def _plot_front_counts(num_fronts_matrix, colour_map_object, output_file_name):
+def _plot_front_counts(
+        num_fronts_matrix, mask_matrix, colour_map_object, output_file_name):
     """Plots number of fronts at each NARR grid cell.
 
     M = number of grid rows (unique y-coordinates at grid points)
@@ -91,6 +105,8 @@ def _plot_front_counts(num_fronts_matrix, colour_map_object, output_file_name):
 
     :param num_fronts_matrix: M-by-N numpy array with number of fronts at each
         grid cell.
+    :param mask_matrix: M-by-N numpy array of integers.  If
+        mask_matrix[i, j] = 0, grid cell [i, j] will be masked out in the map.
     :param colour_map_object: Instance of `matplotlib.pyplot.cm`.
     :param output_file_name: Path to output (image) file.  The figure will be
         saved here.
@@ -119,7 +135,7 @@ def _plot_front_counts(num_fronts_matrix, colour_map_object, output_file_name):
     num_fronts_matrix = num_fronts_matrix.astype(float)
     max_colour_value = numpy.percentile(
         num_fronts_matrix, MAX_PERCENTILE_FOR_COLOUR_MAP)
-    num_fronts_matrix[num_fronts_matrix == 0] = numpy.nan
+    num_fronts_matrix[mask_matrix == 0] = numpy.nan
 
     narr_plotting.plot_xy_grid(
         data_matrix=num_fronts_matrix, axes_object=axes_object,
@@ -129,7 +145,7 @@ def _plot_front_counts(num_fronts_matrix, colour_map_object, output_file_name):
     plotting_utils.add_linear_colour_bar(
         axes_object_or_list=axes_object, values_to_colour=num_fronts_matrix,
         colour_map=colour_map_object, colour_min=0.,
-        colour_max=max_colour_value, orientation='vertical', extend_min=False,
+        colour_max=max_colour_value, orientation='horizontal', extend_min=False,
         extend_max=True)
 
     print 'Saving figure to: "{0:s}"...'.format(output_file_name)
@@ -138,7 +154,7 @@ def _plot_front_counts(num_fronts_matrix, colour_map_object, output_file_name):
 
 
 def _run(top_frontal_grid_dir_name, first_time_string, last_time_string,
-         dilation_distance_metres, output_dir_name):
+         dilation_distance_metres, min_frontal_grid_cells, output_dir_name):
     """Creates mask, indicating where human forecasters usually draw fronts.
 
     This is effectively the main method.
@@ -147,11 +163,13 @@ def _run(top_frontal_grid_dir_name, first_time_string, last_time_string,
     :param first_time_string: Same.
     :param last_time_string: Same.
     :param dilation_distance_metres: Same.
+    :param min_frontal_grid_cells: Same.
     :param output_dir_name: Same.
     """
 
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=output_dir_name)
+    error_checking.assert_is_greater(min_frontal_grid_cells, 0)
 
     first_time_unix_sec = time_conversion.string_to_unix_sec(
         first_time_string, INPUT_TIME_FORMAT)
@@ -208,26 +226,30 @@ def _run(top_frontal_grid_dir_name, first_time_string, last_time_string,
 
     print SEPARATOR_STRING
 
-    pickle_file_name = '{0:s}/front_counts.p'.format(output_dir_name)
-    print 'Writing grids of warm- and cold-front counts to: "{0:s}"...'.format(
-        pickle_file_name)
+    print 'Masking out grid cells with < {0:d} fronts...'.format(
+        min_frontal_grid_cells)
+    mask_matrix = (
+        num_warm_fronts_matrix + num_cold_fronts_matrix >=
+        min_frontal_grid_cells
+    ).astype(int)
 
+    pickle_file_name = '{0:s}/narr_mask.p'.format(output_dir_name)
+    print 'Writing mask to: "{0:s}"...'.format(pickle_file_name)
     pickle_file_handle = open(pickle_file_name, 'wb')
-    pickle.dump(num_cold_fronts_matrix, pickle_file_handle)
-    pickle.dump(num_warm_fronts_matrix, pickle_file_handle)
+    pickle.dump(mask_matrix, pickle_file_handle)
     pickle_file_handle.close()
 
     cold_front_map_file_name = '{0:s}/num_cold_fronts.jpg'.format(
         output_dir_name)
     _plot_front_counts(
-        num_fronts_matrix=num_cold_fronts_matrix,
+        num_fronts_matrix=num_cold_fronts_matrix, mask_matrix=mask_matrix,
         colour_map_object=COLD_FRONT_COLOUR_MAP_OBJECT,
         output_file_name=cold_front_map_file_name)
 
     warm_front_map_file_name = '{0:s}/num_warm_fronts.jpg'.format(
         output_dir_name)
     _plot_front_counts(
-        num_fronts_matrix=num_warm_fronts_matrix,
+        num_fronts_matrix=num_warm_fronts_matrix, mask_matrix=mask_matrix,
         colour_map_object=WARM_FRONT_COLOUR_MAP_OBJECT,
         output_file_name=warm_front_map_file_name)
 
@@ -242,4 +264,6 @@ if __name__ == '__main__':
         last_time_string=getattr(INPUT_ARG_OBJECT, LAST_TIME_ARG_NAME),
         dilation_distance_metres=getattr(
             INPUT_ARG_OBJECT, DILATION_DISTANCE_ARG_NAME),
+        min_frontal_grid_cells=getattr(
+            INPUT_ARG_OBJECT, MIN_FRONTAL_GRID_CELLS_ARG_NAME),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME))
