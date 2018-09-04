1bef067f8e8a0cec86d76a3d07879ed1fce8995d
thunderhoser <ryan.lagerquist@ou.edu>
2018-09-04 16:28:53 -0500

    Fixed bug in process_narr_data.py (need to maintain even more field names).

--- generalexam/scripts/process_narr_data.py
+++ generalexam/scripts/process_narr_data.py
@@ -112,15 +112,15 @@ def _field_name_gg_to_grib1(field_name, pressure_level_mb):
 
 
 def _run(
-        first_time_string, last_time_string, field_name, pressure_level_mb,
-        top_input_narr_dir_name, top_processed_dir_name):
+        first_time_string, last_time_string, input_field_name,
+        pressure_level_mb, top_input_narr_dir_name, top_processed_dir_name):
     """For each time step, processes a single NARR field and writes to file.
 
     This is effectively the main method.
 
     :param first_time_string: See documentation at top of file.
     :param last_time_string: Same.
-    :param field_name: Same.
+    :param input_field_name: Same.
     :param pressure_level_mb: Same.
     :param top_input_narr_dir_name: Same.
     :param top_processed_dir_name: Same.
@@ -140,33 +140,33 @@ def _run(
         t in valid_times_unix_sec
     ]
 
-    if field_name == processed_narr_io.U_WIND_EARTH_RELATIVE_NAME:
-        field_name_other_component = (
+    if input_field_name == processed_narr_io.U_WIND_EARTH_RELATIVE_NAME:
+        input_field_name_other_component = (
             processed_narr_io.V_WIND_EARTH_RELATIVE_NAME
         )
-    elif field_name == processed_narr_io.V_WIND_EARTH_RELATIVE_NAME:
-        field_name_other_component = (
+    elif input_field_name == processed_narr_io.V_WIND_EARTH_RELATIVE_NAME:
+        input_field_name_other_component = (
             processed_narr_io.U_WIND_EARTH_RELATIVE_NAME
         )
     else:
-        field_name_other_component = None
+        input_field_name_other_component = None
 
-    field_name_grib1 = _field_name_gg_to_grib1(
-        field_name=field_name, pressure_level_mb=pressure_level_mb)
+    input_field_name_grib1 = _field_name_gg_to_grib1(
+        field_name=input_field_name, pressure_level_mb=pressure_level_mb)
 
-    rotate_winds = field_name in WIND_FIELD_NAMES
+    rotate_winds = input_field_name in WIND_FIELD_NAMES
     if rotate_winds:
-        field_name_grib1_other_component = _field_name_gg_to_grib1(
-            field_name=field_name_other_component,
+        input_field_name_grib1_other_component = _field_name_gg_to_grib1(
+            field_name=input_field_name_other_component,
             pressure_level_mb=pressure_level_mb)
-    else:
-        field_name_grib1_other_component = None
 
-    num_times = len(valid_times_unix_sec)
-    num_grid_rows, num_grid_columns = nwp_model_utils.get_grid_dimensions(
-        model_name=nwp_model_utils.NARR_MODEL_NAME)
+        output_field_name = processed_narr_io.field_name_to_grid_relative(
+            input_field_name)
+        output_field_name_other_component = (
+            processed_narr_io.field_name_to_grid_relative(
+                input_field_name_other_component)
+        )
 
-    if rotate_winds:
         (grid_point_lat_matrix_deg, grid_point_lng_matrix_deg
         ) = nwp_model_utils.get_latlng_grid_point_matrices(
             model_name=nwp_model_utils.NARR_MODEL_NAME)
@@ -176,6 +176,14 @@ def _run(
             latitudes_deg=grid_point_lat_matrix_deg,
             longitudes_deg=grid_point_lng_matrix_deg,
             model_name=nwp_model_utils.NARR_MODEL_NAME)
+    else:
+        input_field_name_grib1_other_component = None
+        output_field_name = input_field_name + ''
+        output_field_name_other_component = None
+
+    num_times = len(valid_times_unix_sec)
+    num_grid_rows, num_grid_columns = nwp_model_utils.get_grid_dimensions(
+        model_name=nwp_model_utils.NARR_MODEL_NAME)
 
     for i in range(num_times):
         field_matrix = numpy.full(
@@ -190,32 +198,32 @@ def _run(
                 valid_times_unix_sec[i], MONTH_TIME_FORMAT)
 
             this_netcdf_file_name = narr_netcdf_io.find_file(
-                month_string=this_month_string, field_name=field_name,
+                month_string=this_month_string, field_name=input_field_name,
                 top_directory_name=top_input_narr_dir_name)
             if rotate_winds:
                 this_netcdf_file_name_other_component = (
                     narr_netcdf_io.find_file(
                         month_string=this_month_string,
-                        field_name=field_name_other_component,
+                        field_name=input_field_name_other_component,
                         top_directory_name=top_input_narr_dir_name)
                 )
 
             print 'Reading "{0:s}" at {1:d} mb from: "{2:s}"...'.format(
-                field_name, pressure_level_mb, this_netcdf_file_name)
+                input_field_name, pressure_level_mb, this_netcdf_file_name)
             field_matrix[0, ...] = narr_netcdf_io.read_data_from_file(
-                this_netcdf_file_name, field_name=field_name,
+                this_netcdf_file_name, field_name=input_field_name,
                 valid_time_unix_sec=valid_times_unix_sec[i],
                 pressure_level_mb=pressure_level_mb)[0]
 
             if rotate_winds:
                 print 'Reading "{0:s}" at {1:d} mb from: "{2:s}"...'.format(
-                    field_name_other_component, pressure_level_mb,
+                    input_field_name_other_component, pressure_level_mb,
                     this_netcdf_file_name_other_component)
 
                 field_matrix_other_component[0, ...] = (
                     narr_netcdf_io.read_data_from_file(
                         this_netcdf_file_name_other_component,
-                        field_name=field_name_other_component,
+                        field_name=input_field_name_other_component,
                         valid_time_unix_sec=valid_times_unix_sec[i],
                         pressure_level_mb=pressure_level_mb)
                 )[0]
@@ -227,25 +235,25 @@ def _run(
                 top_directory_name=top_input_narr_dir_name)
 
             print 'Reading "{0:s}" at {1:d} mb from: "{2:s}"...'.format(
-                field_name, pressure_level_mb, this_grib_file_name)
+                input_field_name, pressure_level_mb, this_grib_file_name)
 
             field_matrix[0, ...] = nwp_model_io.read_field_from_grib_file(
                 grib_file_name=this_grib_file_name,
-                field_name_grib1=field_name_grib1,
+                field_name_grib1=input_field_name_grib1,
                 model_name=nwp_model_utils.NARR_MODEL_NAME,
                 temporary_dir_name=None, wgrib_exe_name=WGRIB_EXE_NAME,
                 wgrib2_exe_name=WGRIB2_EXE_NAME, raise_error_if_fails=True)
 
             if rotate_winds:
                 print 'Reading "{0:s}" at {1:d} mb from: "{2:s}"...'.format(
-                    field_name_other_component, pressure_level_mb,
+                    input_field_name_other_component, pressure_level_mb,
                     this_grib_file_name)
 
                 field_matrix_other_component[
                     0, ...
                 ] = nwp_model_io.read_field_from_grib_file(
                     grib_file_name=this_grib_file_name,
-                    field_name_grib1=field_name_grib1_other_component,
+                    field_name_grib1=input_field_name_grib1_other_component,
                     model_name=nwp_model_utils.NARR_MODEL_NAME,
                     temporary_dir_name=None, wgrib_exe_name=WGRIB_EXE_NAME,
                     wgrib2_exe_name=WGRIB2_EXE_NAME, raise_error_if_fails=True)
@@ -256,7 +264,7 @@ def _run(
             print ('Rotating winds at {0:s} from Earth-relative to grid-'
                    'relative...').format(valid_time_strings[i])
 
-            if field_name == processed_narr_io.U_WIND_EARTH_RELATIVE_NAME:
+            if input_field_name == processed_narr_io.U_WIND_EARTH_RELATIVE_NAME:
                 (field_matrix[0, ...], field_matrix_other_component[0, ...]
                 ) = nwp_model_utils.rotate_winds_to_grid_relative(
                     u_winds_earth_relative_m_s01=field_matrix[0, ...],
@@ -273,44 +281,37 @@ def _run(
                     rotation_angle_cosines=rotation_angle_cos_matrix,
                     rotation_angle_sines=rotation_angle_sin_matrix)
 
-            field_name = processed_narr_io.field_name_to_grid_relative(
-                field_name)
-            field_name_other_component = (
-                processed_narr_io.field_name_to_grid_relative(
-                    field_name_other_component)
-            )
-
         this_processed_file_name = processed_narr_io.find_file_for_one_time(
-            top_directory_name=top_processed_dir_name, field_name=field_name,
-            pressure_level_mb=pressure_level_mb,
+            top_directory_name=top_processed_dir_name,
+            field_name=output_field_name, pressure_level_mb=pressure_level_mb,
             valid_time_unix_sec=valid_times_unix_sec[i],
             raise_error_if_missing=False)
 
         print 'Writing "{0:s}" at {1:d} mb and {2:s} to: "{3:s}"...'.format(
-            field_name, pressure_level_mb, valid_time_strings[i],
+            output_field_name, pressure_level_mb, valid_time_strings[i],
             this_processed_file_name)
         processed_narr_io.write_fields_to_file(
             pickle_file_name=this_processed_file_name,
-            field_matrix=field_matrix, field_name=field_name,
+            field_matrix=field_matrix, field_name=output_field_name,
             pressure_level_pascals=pressure_level_mb * MB_TO_PASCALS,
             valid_times_unix_sec=numpy.array([valid_times_unix_sec[0]]))
 
         if rotate_winds:
             this_processed_file_name = processed_narr_io.find_file_for_one_time(
                 top_directory_name=top_processed_dir_name,
-                field_name=field_name_other_component,
+                field_name=output_field_name_other_component,
                 pressure_level_mb=pressure_level_mb,
                 valid_time_unix_sec=valid_times_unix_sec[i],
                 raise_error_if_missing=False)
 
             print 'Writing "{0:s}" at {1:d} mb and {2:s} to: "{3:s}"...'.format(
-                field_name_other_component, pressure_level_mb,
+                output_field_name_other_component, pressure_level_mb,
                 valid_time_strings[i], this_processed_file_name)
 
             processed_narr_io.write_fields_to_file(
                 pickle_file_name=this_processed_file_name,
                 field_matrix=field_matrix_other_component,
-                field_name=field_name_other_component,
+                field_name=output_field_name_other_component,
                 pressure_level_pascals=pressure_level_mb * MB_TO_PASCALS,
                 valid_times_unix_sec=numpy.array([valid_times_unix_sec[0]]))
 
@@ -321,7 +322,7 @@ if __name__ == '__main__':
     _run(
         first_time_string=getattr(INPUT_ARG_OBJECT, FIRST_TIME_ARG_NAME),
         last_time_string=getattr(INPUT_ARG_OBJECT, LAST_TIME_ARG_NAME),
-        field_name=getattr(INPUT_ARG_OBJECT, FIELD_NAME_ARG_NAME),
+        input_field_name=getattr(INPUT_ARG_OBJECT, FIELD_NAME_ARG_NAME),
         pressure_level_mb=getattr(INPUT_ARG_OBJECT, PRESSURE_LEVEL_ARG_NAME),
         top_input_narr_dir_name=getattr(INPUT_ARG_OBJECT, NARR_DIR_ARG_NAME),
         top_processed_dir_name=getattr(INPUT_ARG_OBJECT, PROCESSED_DIR_ARG_NAME)
