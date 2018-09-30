8fc4705b75e9b920419df9d5acd9956d67ffb351
thunderhoser <ryan.lagerquist@ou.edu>
2018-09-30 17:54:23 -0500

    Added input arg to shuffle_downsized_3d_files.py.

--- generalexam/scripts/shuffle_downsized_3d_files.py
+++ generalexam/scripts/shuffle_downsized_3d_files.py
@@ -16,7 +16,8 @@ FIRST_TIME_ARG_NAME = 'first_time_string'
 LAST_TIME_ARG_NAME = 'last_time_string'
 NUM_EXAMPLES_PER_CHUNK_ARG_NAME = 'num_examples_per_chunk'
 OUTPUT_DIR_ARG_NAME = 'top_output_dir_name'
-NUM_EXAMPLES_PER_OUT_FILE_ARG_NAME = 'num_examples_per_out_file'
+NUM_OUTPUT_FILES_ARG_NAME = 'num_output_files'
+NUM_EXAMPLES_PER_FILE_ARG_NAME = 'num_examples_per_out_file'
 
 INPUT_DIR_HELP_STRING = (
     'Name of input directory.  Files therein will be found by '
@@ -39,11 +40,18 @@ OUTPUT_DIR_HELP_STRING = (
     '`training_validation_io.write_downsized_3d_examples` to locations therein,'
     ' determined by `training_validation_io.find_downsized_3d_example_file`.')
 
-NUM_EXAMPLES_PER_OUT_FILE_HELP_STRING = (
-    'Number of examples in each randomly shuffled output file.')
+NUM_OUTPUT_FILES_HELP_STRING = (
+    'Number of output files.  If you want to specify number of examples per '
+    'output file, rather than number of output files, set `{0:s}`.  Only one of'
+    ' these args will be used.'
+).format(NUM_EXAMPLES_PER_FILE_ARG_NAME)
+
+NUM_EXAMPLES_PER_FILE_HELP_STRING = 'See doc for `{0:s}`.'.format(
+    NUM_OUTPUT_FILES_ARG_NAME)
 
 DEFAULT_NUM_EXAMPLES_PER_CHUNK = 8
-DEFAULT_NUM_EXAMPLES_PER_OUT_FILE = 1024
+DEFAULT_NUM_OUTPUT_FILES = -1
+DEFAULT_NUM_EXAMPLES_PER_FILE = 1024
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
@@ -66,17 +74,25 @@ INPUT_ARG_PARSER.add_argument(
     help=OUTPUT_DIR_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + NUM_EXAMPLES_PER_OUT_FILE_ARG_NAME, type=int, required=False,
-    default=DEFAULT_NUM_EXAMPLES_PER_OUT_FILE,
-    help=NUM_EXAMPLES_PER_OUT_FILE_HELP_STRING)
+    '--' + NUM_OUTPUT_FILES_ARG_NAME, type=int, required=False,
+    default=DEFAULT_NUM_OUTPUT_FILES, help=NUM_OUTPUT_FILES_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_EXAMPLES_PER_FILE_ARG_NAME, type=int, required=False,
+    default=DEFAULT_NUM_EXAMPLES_PER_FILE,
+    help=NUM_EXAMPLES_PER_FILE_HELP_STRING)
 
 
-def _find_input_files(input_dir_name, first_time_unix_sec, last_time_unix_sec):
+def _find_input_files(
+        input_dir_name, first_time_unix_sec, last_time_unix_sec,
+        return_num_examples_total):
     """Finds input files.
 
     :param input_dir_name: See documentation at top of file.
     :param first_time_unix_sec: Same.
     :param last_time_unix_sec: Same.
+    :param return_num_examples_total: Boolean flag.  If True,
+        `num_examples_total` will be an integer.  If False, it will be None.
     :return: input_file_names: 1-D list of paths to input files.
     :return: num_examples_total: Number of examples among all input files.
     """
@@ -87,8 +103,10 @@ def _find_input_files(input_dir_name, first_time_unix_sec, last_time_unix_sec):
         last_target_time_unix_sec=last_time_unix_sec)
 
     num_input_files = len(input_file_names)
-    num_examples_total = 0
+    if not return_num_examples_total:
+        return input_file_names, None
 
+    num_examples_total = 0
     for i in range(num_input_files):
         print 'Reading data from: "{0:s}"...'.format(input_file_names[i])
         this_example_dict = trainval_io.read_downsized_3d_examples(
@@ -99,26 +117,33 @@ def _find_input_files(input_dir_name, first_time_unix_sec, last_time_unix_sec):
         num_examples_total += len(
             this_example_dict[trainval_io.TARGET_TIMES_KEY])
 
+    print SEPARATOR_STRING
     return input_file_names, num_examples_total
 
 
 def _set_output_locations(
-        num_examples_total, top_output_dir_name, num_examples_per_out_file):
+        top_output_dir_name, num_output_files=None, num_examples_total=None,
+        num_examples_per_out_file=None):
     """Determines locations of output files.
 
-    :param num_examples_total: Number of examples among all input files.
     :param top_output_dir_name: See documentation at top of file.
-    :param num_examples_per_out_file: Same.
+    :param num_output_files: Same.
+    :param num_examples_total: [used iff `num_output_files is None`]
+        Number of examples among all input files.
+    :param num_examples_per_out_file: [used iff `num_output_files is None`]
+        Number of examples per output file.
     :return: output_file_names: 1-D list of paths to output files.
     """
 
-    num_output_files = int(
-        numpy.ceil(float(num_examples_total) / num_examples_per_out_file)
-    )
-    print (
-        'Number of examples = {0:d} ... number of examples per output file = '
-        '{1:d} ... number of output files = {2:d}'
-    ).format(num_examples_total, num_examples_per_out_file, num_output_files)
+    if num_output_files is None:
+        num_output_files = int(
+            numpy.ceil(float(num_examples_total) / num_examples_per_out_file)
+        )
+
+        print (
+            'Number of examples = {0:d} ... number of examples per output file '
+            '= {1:d} ... number of output files = {2:d}'
+        ).format(num_examples_total, num_examples_per_out_file, num_output_files)
 
     output_file_names = [
         trainval_io.find_downsized_3d_example_file(
@@ -127,11 +152,11 @@ def _set_output_locations(
         ) for i in range(num_output_files)
     ]
 
-    for this_file_name in output_file_names:
-        if not os.path.isfile(this_file_name):
-            continue
-        print 'Deleting output file: "{0:s}"...'.format(this_file_name)
-        os.remove(this_file_name)
+    # for this_file_name in output_file_names:
+    #     if not os.path.isfile(this_file_name):
+    #         continue
+    #     print 'Deleting output file: "{0:s}"...'.format(this_file_name)
+    #     os.remove(this_file_name)
 
     return output_file_names
 
@@ -196,7 +221,7 @@ def _shuffle_one_input_file(
 
 
 def _run(input_dir_name, first_time_string, last_time_string,
-         num_examples_per_chunk, top_output_dir_name,
+         num_examples_per_chunk, top_output_dir_name, num_output_files,
          num_examples_per_out_file):
     """Randomly shuffles downsized 3-D examples among files.
 
@@ -207,8 +232,12 @@ def _run(input_dir_name, first_time_string, last_time_string,
     :param last_time_string: Same.
     :param num_examples_per_chunk: Same.
     :param top_output_dir_name: Same.
+    :param num_output_files: Same.
     :param num_examples_per_out_file: Same.
     """
+
+    if num_output_files <= 0:
+        num_output_files = None
 
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=top_output_dir_name)
@@ -222,15 +251,14 @@ def _run(input_dir_name, first_time_string, last_time_string,
 
     input_file_names, num_examples_total = _find_input_files(
         input_dir_name=input_dir_name, first_time_unix_sec=first_time_unix_sec,
-        last_time_unix_sec=last_time_unix_sec)
+        last_time_unix_sec=last_time_unix_sec,
+        return_num_examples_total=num_output_files is None)
     num_input_files = len(input_file_names)
-    print SEPARATOR_STRING
 
     output_file_names = _set_output_locations(
         num_examples_total=num_examples_total,
         top_output_dir_name=top_output_dir_name,
         num_examples_per_out_file=num_examples_per_out_file)
-    print SEPARATOR_STRING
 
     for i in range(num_input_files):
         _shuffle_one_input_file(
@@ -252,6 +280,7 @@ if __name__ == '__main__':
         num_examples_per_chunk=getattr(
             INPUT_ARG_OBJECT, NUM_EXAMPLES_PER_CHUNK_ARG_NAME),
         top_output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME),
+        num_output_files=getattr(INPUT_ARG_OBJECT, NUM_OUTPUT_FILES_ARG_NAME),
         num_examples_per_out_file=getattr(
-            INPUT_ARG_OBJECT, NUM_EXAMPLES_PER_OUT_FILE_ARG_NAME)
+            INPUT_ARG_OBJECT, NUM_EXAMPLES_PER_FILE_ARG_NAME)
     )
