39b2eb35c529e8abc01ce1b776a1c5452a456ccf
thunderhoser <ryan.lagerquist@ou.edu>
2018-10-11 18:04:46 -0500

    Tweaked plotting scripts.

--- generalexam/figures/plot_cnn_experiment.py
+++ generalexam/figures/plot_cnn_experiment.py
@@ -200,10 +200,8 @@ def _run():
     for k in range(num_dropout_fractions):
         if k == 0:
             this_y_axis_text_colour = BLACK_COLOUR + 0.
-            these_y_tick_labels = UNIQUE_PREDICTOR_ABBREV_STRINGS + []
         else:
             this_y_axis_text_colour = WHITE_COLOUR + 0.
-            these_y_tick_labels = [''] * num_predictor_combos
         
         this_title_string = 'Gerrity score; dropout = {0:.2f}'.format(
             UNIQUE_DROPOUT_FRACTIONS[k])
@@ -221,7 +219,7 @@ def _run():
             x_tick_labels=UNIQUE_IMAGE_SIZE_STRINGS,
             x_axis_label=IMAGE_SIZE_AXIS_LABEL,
             x_axis_text_colour=WHITE_COLOUR,
-            y_tick_labels=these_y_tick_labels,
+            y_tick_labels=UNIQUE_PREDICTOR_ABBREV_STRINGS,
             y_axis_label='',
             y_axis_text_colour=this_y_axis_text_colour,
             title_string=this_title_string,
@@ -243,7 +241,7 @@ def _run():
             x_tick_labels=UNIQUE_IMAGE_SIZE_STRINGS,
             x_axis_label=IMAGE_SIZE_AXIS_LABEL,
             x_axis_text_colour=WHITE_COLOUR,
-            y_tick_labels=these_y_tick_labels,
+            y_tick_labels=UNIQUE_PREDICTOR_ABBREV_STRINGS,
             y_axis_label='',
             y_axis_text_colour=this_y_axis_text_colour,
             title_string=this_title_string,
@@ -265,7 +263,7 @@ def _run():
             x_tick_labels=UNIQUE_IMAGE_SIZE_STRINGS,
             x_axis_label=IMAGE_SIZE_AXIS_LABEL,
             x_axis_text_colour=WHITE_COLOUR,
-            y_tick_labels=these_y_tick_labels,
+            y_tick_labels=UNIQUE_PREDICTOR_ABBREV_STRINGS,
             y_axis_label='',
             y_axis_text_colour=this_y_axis_text_colour,
             title_string=this_title_string,
@@ -287,7 +285,7 @@ def _run():
             x_tick_labels=UNIQUE_IMAGE_SIZE_STRINGS,
             x_axis_label=IMAGE_SIZE_AXIS_LABEL,
             x_axis_text_colour=BLACK_COLOUR,
-            y_tick_labels=these_y_tick_labels,
+            y_tick_labels=UNIQUE_PREDICTOR_ABBREV_STRINGS,
             y_axis_label='',
             y_axis_text_colour=this_y_axis_text_colour,
             title_string=this_title_string,
