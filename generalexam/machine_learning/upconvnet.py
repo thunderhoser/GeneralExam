b96e23318e99bddef536ec24b046062dc248cb1b
thunderhoser <lagerqui@ualberta.ca>
2018-12-28 06:47:46 +0000

    Added generator and training method to upconvnet.py.

--- generalexam/machine_learning/upconvnet.py
+++ generalexam/machine_learning/upconvnet.py
@@ -1,8 +1,12 @@
 """Methods for setting up, training, and applying upconvolution networks."""
 
+from random import shuffle
 import numpy
 import keras
+from gewittergefahr.gg_utils import file_system_utils
 from gewittergefahr.gg_utils import error_checking
+from gewittergefahr.deep_learning import cnn
+from generalexam.machine_learning import training_validation_io as trainval_io
 
 # TODO(thunderhoser): This code contains a lot of hacks, including constants
 # that shouldn't really be constants.
@@ -14,6 +18,10 @@ NUM_CONV_FILTER_ROWS = 5
 NUM_CONV_FILTER_COLUMNS = 5
 # NUM_CONV_FILTER_ROWS = 3
 # NUM_CONV_FILTER_COLUMNS = 3
+
+LARGE_INTEGER = int(1e10)
+MIN_MSE_DECREASE_FOR_EARLY_STOP = 0.005
+NUM_EPOCHS_FOR_EARLY_STOP = 5
 
 
 def create_net(
@@ -148,3 +156,209 @@ def create_net(
 
     model_object.summary()
     return model_object
+
+
+def trainval_generator(
+        top_input_dir_name, first_time_unix_sec, last_time_unix_sec,
+        narr_predictor_names, num_half_rows, num_half_columns,
+        num_examples_per_batch, cnn_model_object, cnn_feature_layer_name):
+    """Generates training or validation examples for upconvnet on the fly.
+
+    E = number of examples
+    M = number of rows in each grid
+    N = number of columns in each grid
+    C = number of channels (predictor variables)
+    Z = number of scalar features (neurons in layer `cnn_feature_layer_name` of
+        the CNN specified by `cnn_model_object`)
+
+    :param top_input_dir_name: Name of top-level directory with downsized 3-D
+        examples (two spatial dimensions).  Files therein will be found by
+        `training_validation_io.find_downsized_3d_example_file` (with
+        `shuffled = True`) and read by
+        `training_validation_io.read_downsized_3d_examples`.
+    :param first_time_unix_sec: First valid time.  Only examples with valid time
+        in `first_time_unix_sec`...`last_time_unix_sec` will be kept.
+    :param last_time_unix_sec: See above.
+    :param narr_predictor_names: See doc for
+        `training_validation_io.read_downsized_3d_examples`.
+    :param num_half_rows: See doc for
+        `training_validation_io.read_downsized_3d_examples`.
+    :param num_half_columns: Same.
+    :param num_examples_per_batch: Number of examples in each batch.
+    :param cnn_model_object: Trained CNN model (instance of
+        `keras.models.Model`).  This will be used to turn images stored in
+        `top_input_dir_name` into scalar features.
+    :param cnn_feature_layer_name: The "scalar features" will be the set of
+        activations from this layer.
+    :return: feature_matrix: E-by-Z numpy array of scalar features.  These are
+        the "predictors" for the upconv network.
+    :return: target_matrix: E-by-M-by-N-by-C numpy array of target images.
+        These are the predictors for the CNN and the targets for the upconv
+        network.
+    """
+
+    error_checking.assert_is_integer(num_examples_per_batch)
+    error_checking.assert_is_geq(num_examples_per_batch, 10)
+
+    partial_cnn_model_object = cnn.model_to_feature_generator(
+        model_object=cnn_model_object, output_layer_name=cnn_feature_layer_name)
+
+    example_file_names = trainval_io.find_downsized_3d_example_files(
+        top_directory_name=top_input_dir_name, shuffled=True,
+        first_batch_number=0, last_batch_number=LARGE_INTEGER)
+    shuffle(example_file_names)
+
+    num_files = len(example_file_names)
+    file_index = 0
+    batch_indices = numpy.linspace(
+        0, num_examples_per_batch - 1, num=num_examples_per_batch, dtype=int)
+
+    num_examples_in_memory = 0
+    full_target_matrix = None
+
+    while True:
+        while num_examples_in_memory < num_examples_per_batch:
+            print 'Reading data from: "{0:s}"...'.format(
+                example_file_names[file_index])
+
+            this_example_dict = trainval_io.read_downsized_3d_examples(
+                netcdf_file_name=example_file_names[file_index],
+                predictor_names_to_keep=narr_predictor_names,
+                num_half_rows_to_keep=num_half_rows,
+                num_half_columns_to_keep=num_half_columns,
+                first_time_to_keep_unix_sec=first_time_unix_sec,
+                last_time_to_keep_unix_sec=last_time_unix_sec)
+
+            file_index += 1
+            if file_index >= num_files:
+                file_index = 0
+
+            this_num_examples = len(
+                this_example_dict[trainval_io.TARGET_TIMES_KEY]
+            )
+            if this_num_examples == 0:
+                continue
+
+            if full_target_matrix is None or full_target_matrix.size == 0:
+                full_target_matrix = (
+                    this_example_dict[trainval_io.PREDICTOR_MATRIX_KEY] + 0.
+                )
+            else:
+                full_target_matrix = numpy.concatenate(
+                    (full_target_matrix,
+                     this_example_dict[trainval_io.PREDICTOR_MATRIX_KEY]),
+                    axis=0)
+
+            num_examples_in_memory = full_target_matrix.shape[0]
+
+        target_matrix = full_target_matrix[batch_indices, ...].astype('float32')
+        feature_matrix = partial_cnn_model_object.predict(
+            target_matrix, batch_size=num_examples_per_batch)
+
+        num_examples_in_memory = 0
+        full_target_matrix = None
+
+        yield (feature_matrix, target_matrix)
+
+
+def train_upconvnet(
+        ucn_model_object, top_training_dir_name, first_training_time_unix_sec,
+        last_training_time_unix_sec, narr_predictor_names, num_half_rows,
+        num_half_columns, num_examples_per_batch, cnn_model_object,
+        cnn_feature_layer_name, num_epochs, num_training_batches_per_epoch,
+        output_model_file_name, num_validation_batches_per_epoch=None,
+        top_validation_dir_name=None, first_validation_time_unix_sec=None,
+        last_validation_time_unix_sec=None):
+    """Trains upconvnet.
+
+    :param ucn_model_object: Untrained instance of `keras.models.Model`,
+        representing the upconv network.
+    :param top_training_dir_name: Training data will be found here.  See doc for
+        input `top_input_dir_name` to method `training_generator`.
+    :param first_training_time_unix_sec: Determines training period.  See doc
+        for input `first_time_unix_sec` to method `training_generator`.
+    :param last_training_time_unix_sec: Determines training period.  See doc for
+        input `last_time_unix_sec` to method `training_generator`.
+    :param narr_predictor_names: See doc for `training_generator`.
+    :param num_half_rows: Same.
+    :param num_half_columns: Same.
+    :param num_examples_per_batch: Same.
+    :param cnn_model_object: Same.
+    :param cnn_feature_layer_name: Same.
+    :param num_epochs: Number of epochs.
+    :param num_training_batches_per_epoch: Number of training batches furnished
+        to model in each epoch.
+    :param output_model_file_name: Path to output file.  The model will be saved
+        as an HDF5 file (extension should be ".h5", but this is not enforced).
+    :param num_validation_batches_per_epoch: Number of validation batches
+        furnished to model in each epoch.  If
+        `num_validation_batches_per_epoch is None`, there will be no on-the-fly
+        validation.
+    :param top_validation_dir_name:
+        [used only if `num_validation_batches_per_epoch is not None`]
+        Validation data will be found here.  See doc for input
+        `top_input_dir_name` to method `training_generator`.
+    :param first_validation_time_unix_sec:
+        [used only if `num_validation_batches_per_epoch is not None`]
+        Determines validation period.  See doc for input `first_time_unix_sec`
+        to method `training_generator`.
+    :param last_validation_time_unix_sec:
+        [used only if `num_validation_batches_per_epoch is not None`]
+        Determines validation period.  See doc for input `last_time_unix_sec`
+        to method `training_generator`.
+    """
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        file_name=output_model_file_name)
+
+    if num_validation_batches_per_epoch is None:
+        checkpoint_object = keras.callbacks.ModelCheckpoint(
+            output_model_file_name, monitor='loss', verbose=1,
+            save_best_only=False, save_weights_only=False, mode='min', period=1)
+    else:
+        checkpoint_object = keras.callbacks.ModelCheckpoint(
+            output_model_file_name, monitor='val_loss', verbose=1,
+            save_best_only=True, save_weights_only=False, mode='min', period=1)
+
+    list_of_callback_objects = [checkpoint_object]
+
+    training_generator = trainval_generator(
+        top_input_dir_name=top_training_dir_name,
+        first_time_unix_sec=first_training_time_unix_sec,
+        last_time_unix_sec=last_training_time_unix_sec,
+        narr_predictor_names=narr_predictor_names, num_half_rows=num_half_rows,
+        num_half_columns=num_half_columns,
+        num_examples_per_batch=num_examples_per_batch,
+        cnn_model_object=cnn_model_object,
+        cnn_feature_layer_name=cnn_feature_layer_name)
+
+    if num_validation_batches_per_epoch is None:
+        ucn_model_object.fit_generator(
+            generator=training_generator,
+            steps_per_epoch=num_training_batches_per_epoch, epochs=num_epochs,
+            verbose=1, callbacks=list_of_callback_objects)
+
+        return
+
+    early_stopping_object = keras.callbacks.EarlyStopping(
+        monitor='val_loss', min_delta=MIN_MSE_DECREASE_FOR_EARLY_STOP,
+        patience=NUM_EPOCHS_FOR_EARLY_STOP, verbose=1, mode='min')
+
+    list_of_callback_objects.append(early_stopping_object)
+
+    validation_generator = trainval_generator(
+        top_input_dir_name=top_validation_dir_name,
+        first_time_unix_sec=first_validation_time_unix_sec,
+        last_time_unix_sec=last_validation_time_unix_sec,
+        narr_predictor_names=narr_predictor_names, num_half_rows=num_half_rows,
+        num_half_columns=num_half_columns,
+        num_examples_per_batch=num_examples_per_batch,
+        cnn_model_object=cnn_model_object,
+        cnn_feature_layer_name=cnn_feature_layer_name)
+
+    ucn_model_object.fit_generator(
+        generator=training_generator,
+        steps_per_epoch=num_training_batches_per_epoch, epochs=num_epochs,
+        verbose=1, callbacks=list_of_callback_objects,
+        validation_data=validation_generator,
+        validation_steps=num_validation_batches_per_epoch)
