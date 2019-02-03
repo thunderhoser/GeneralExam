f92e8ea359c399cdf94087ad460d93042b0fc6f8
thunderhoser <ryan.lagerquist@ou.edu>
2019-02-02 21:58:29 -0600

    plot_input_examples.py can now plot fronts.

--- generalexam/scripts/plot_input_examples.py
+++ generalexam/scripts/plot_input_examples.py
@@ -12,8 +12,11 @@ from gewittergefahr.gg_utils import error_checking
 from gewittergefahr.plotting import plotting_utils
 from gewittergefahr.plotting import nwp_plotting
 from generalexam.ge_io import processed_narr_io
+from generalexam.ge_io import fronts_io
+from generalexam.ge_utils import front_utils
 from generalexam.machine_learning import training_validation_io as trainval_io
 from generalexam.machine_learning import machine_learning_utils as ml_utils
+from generalexam.plotting import front_plotting
 
 random.seed(6695)
 numpy.random.seed(6695)
@@ -28,6 +31,9 @@ NARR_PREDICTOR_NAMES = [
     processed_narr_io.V_WIND_GRID_RELATIVE_NAME
 ]
 
+WARM_FRONT_COLOUR = numpy.array([217, 95, 2], dtype=float) / 255
+COLD_FRONT_COLOUR = numpy.array([117, 112, 179], dtype=float) / 255
+
 WIND_COLOUR_MAP_OBJECT = pyplot.cm.binary
 WIND_BARB_LENGTH = 8
 EMPTY_WIND_BARB_RADIUS = 0.1
@@ -38,11 +44,10 @@ PARALLEL_SPACING_DEG = 2.
 MERIDIAN_SPACING_DEG = 4.
 BORDER_WIDTH = 2
 BORDER_COLOUR = numpy.full(3, 152. / 255)
-
-TITLE_FONT_SIZE = 20
 FIGURE_RESOLUTION_DPI = 300
 
 INPUT_FILE_ARG_NAME = 'input_example_file_name'
+FRONT_DIR_ARG_NAME = 'input_front_line_dir_name'
 NUM_EXAMPLES_ARG_NAME = 'num_examples'
 EXAMPLE_INDICES_ARG_NAME = 'example_indices'
 COLOUR_MAP_ARG_NAME = 'thetaw_colour_map_name'
@@ -52,6 +57,11 @@ OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 INPUT_FILE_HELP_STRING = (
     'Path to input file.  Will be read by '
     '`training_validation_io.read_downsized_3d_examples`.')
+
+FRONT_DIR_HELP_STRING = (
+    'Name of top-level directory with frontal polylines.  Files therein will be'
+    ' found by `fronts_io.find_file_for_one_time` and read by '
+    '`fronts_io.read_polylines_from_file`.')
 
 NUM_EXAMPLES_HELP_STRING = (
     'Number of examples to draw randomly from `{0:s}`.  To plot all examples, '
@@ -79,10 +89,16 @@ MAX_PERCENTILE_HELP_STRING = (
 OUTPUT_DIR_HELP_STRING = (
     'Name of output directory.  Figures will be saved here.')
 
+TOP_FRONT_DIR_NAME_DEFAULT = '/condo/swatwork/ralager/fronts/polylines'
+
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + INPUT_FILE_ARG_NAME, type=str, required=True,
     help=INPUT_FILE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + FRONT_DIR_ARG_NAME, type=str, required=False,
+    default=TOP_FRONT_DIR_NAME_DEFAULT, help=FRONT_DIR_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + NUM_EXAMPLES_ARG_NAME, type=int, required=False, default=-1,
@@ -93,7 +109,7 @@ INPUT_ARG_PARSER.add_argument(
     default=[-1], help=EXAMPLE_INDICES_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + COLOUR_MAP_ARG_NAME, type=str, required=False, default='plasma',
+    '--' + COLOUR_MAP_ARG_NAME, type=str, required=False, default='YlGn',
     help=COLOUR_MAP_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
@@ -105,13 +121,15 @@ INPUT_ARG_PARSER.add_argument(
     help=OUTPUT_DIR_HELP_STRING)
 
 
-def _run(input_file_name, num_examples, example_indices, thetaw_colour_map_name,
-         thetaw_max_colour_percentile, output_dir_name):
+def _run(example_file_name, top_front_line_dir_name, num_examples,
+         example_indices, thetaw_colour_map_name, thetaw_max_colour_percentile,
+         output_dir_name):
     """Plots one or more input examples.
 
     This is effectively the main method.
 
-    :param input_file_name: See documentation at top of file.
+    :param example_file_name: See documentation at top of file.
+    :param top_front_line_dir_name: Same.
     :param num_examples: Same.
     :param example_indices: Same.
     :param thetaw_colour_map_name: Same.
@@ -134,9 +152,11 @@ def _run(input_file_name, num_examples, example_indices, thetaw_colour_map_name,
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=output_dir_name)
 
-    print 'Reading normalized examples from: "{0:s}"...'.format(input_file_name)
+    print 'Reading normalized examples from: "{0:s}"...'.format(
+        example_file_name)
+
     example_dict = trainval_io.read_downsized_3d_examples(
-        netcdf_file_name=input_file_name, num_half_rows_to_keep=NUM_HALF_ROWS,
+        netcdf_file_name=example_file_name, num_half_rows_to_keep=NUM_HALF_ROWS,
         num_half_columns_to_keep=NUM_HALF_COLUMNS,
         predictor_names_to_keep=NARR_PREDICTOR_NAMES)
 
@@ -191,7 +211,8 @@ def _run(input_file_name, num_examples, example_indices, thetaw_colour_map_name,
         this_first_row_index = this_center_row_index - NUM_HALF_ROWS
         this_last_row_index = this_center_row_index + NUM_HALF_ROWS
 
-        this_center_column_index = example_dict[trainval_io.COLUMN_INDICES_KEY][i]
+        this_center_column_index = example_dict[
+            trainval_io.COLUMN_INDICES_KEY][i]
         this_first_column_index = this_center_column_index - NUM_HALF_COLUMNS
         this_last_column_index = this_center_column_index + NUM_HALF_COLUMNS
 
@@ -208,13 +229,13 @@ def _run(input_file_name, num_examples, example_indices, thetaw_colour_map_name,
             this_first_column_index:(this_last_column_index + 1)
         ]
 
-        # this_u_wind_matrix_m_s01, this_v_wind_matrix_m_s01 = (
-        #     nwp_model_utils.rotate_winds_to_earth_relative(
-        #         u_winds_grid_relative_m_s01=this_u_wind_matrix_m_s01,
-        #         v_winds_grid_relative_m_s01=this_v_wind_matrix_m_s01,
-        #         rotation_angle_cosines=this_cos_matrix,
-        #         rotation_angle_sines=this_sin_matrix)
-        # )
+        this_u_wind_matrix_m_s01, this_v_wind_matrix_m_s01 = (
+            nwp_model_utils.rotate_winds_to_earth_relative(
+                u_winds_grid_relative_m_s01=this_u_wind_matrix_m_s01,
+                v_winds_grid_relative_m_s01=this_v_wind_matrix_m_s01,
+                rotation_angle_cosines=this_cos_matrix,
+                rotation_angle_sines=this_sin_matrix)
+        )
 
         _, axes_object, basemap_object = nwp_plotting.init_basemap(
             model_name=nwp_model_utils.NARR_MODEL_NAME,
@@ -279,6 +300,34 @@ def _run(input_file_name, num_examples, example_indices, thetaw_colour_map_name,
             colour_minimum_kt=MIN_COLOUR_WIND_SPEED_KT,
             colour_maximum_kt=MAX_COLOUR_WIND_SPEED_KT)
 
+        this_front_file_name = fronts_io.find_file_for_one_time(
+            top_directory_name=top_front_line_dir_name,
+            file_type=fronts_io.POLYLINE_FILE_TYPE,
+            valid_time_unix_sec=example_dict[trainval_io.TARGET_TIMES_KEY][i]
+        )
+
+        this_polyline_table = fronts_io.read_polylines_from_file(
+            this_front_file_name)
+        this_num_fronts = len(this_polyline_table.index)
+
+        for j in range(this_num_fronts):
+            this_front_type_string = this_polyline_table[
+                front_utils.FRONT_TYPE_COLUMN].values[j]
+
+            if this_front_type_string == front_utils.WARM_FRONT_STRING_ID:
+                this_colour = WARM_FRONT_COLOUR + 0.
+            else:
+                this_colour = COLD_FRONT_COLOUR + 0.
+
+            front_plotting.plot_polyline(
+                latitudes_deg=this_polyline_table[
+                    front_utils.LATITUDES_COLUMN].values[j],
+                longitudes_deg=this_polyline_table[
+                    front_utils.LONGITUDES_COLUMN].values[j],
+                basemap_object=basemap_object, axes_object=axes_object,
+                front_type=this_front_type_string, line_colour=this_colour,
+                line_width=4)
+
         this_output_file_name = '{0:s}/example{1:06d}.jpg'.format(
             output_dir_name, i)
 
@@ -291,7 +340,8 @@ if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
     _run(
-        input_file_name=getattr(INPUT_ARG_OBJECT, INPUT_FILE_ARG_NAME),
+        example_file_name=getattr(INPUT_ARG_OBJECT, INPUT_FILE_ARG_NAME),
+        top_front_line_dir_name=getattr(INPUT_ARG_OBJECT, FRONT_DIR_ARG_NAME),
         num_examples=getattr(INPUT_ARG_OBJECT, NUM_EXAMPLES_ARG_NAME),
         example_indices=numpy.array(
             getattr(INPUT_ARG_OBJECT, EXAMPLE_INDICES_ARG_NAME), dtype=int),
