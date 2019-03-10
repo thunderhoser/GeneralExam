5b23f3c210fd3d6eaf15a8e98b59083db493a2e3
thunderhoser <ryan.lagerquist@ou.edu>
2019-03-09 18:52:59 -0600

    Added debugging print statement to fronts_io.py.

--- generalexam/ge_io/fronts_io.py
+++ generalexam/ge_io/fronts_io.py
@@ -418,7 +418,8 @@ def read_grid_from_file(netcdf_file_name):
 
     gridded_label_table = gridded_label_table.assign(**argument_dict)
 
-    print dataset_object.variables
+    print dataset_object.variables[front_utils.WARM_FRONT_ROWS_COLUMN]
+    print dir(dataset_object.variables[front_utils.WARM_FRONT_ROWS_COLUMN])
 
     cold_front_rows = numpy.round(numpy.array(
         dataset_object.variables[front_utils.COLD_FRONT_ROWS_COLUMN][:]
