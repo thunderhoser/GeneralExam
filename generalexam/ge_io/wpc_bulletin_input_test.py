fa7f307d290c11a159e1a8c75b2faeec81dc7441
thunderhoser <ryan.lagerquist@ou.edu>
2019-03-19 01:55:57 -0500

    Fixed unit tests in wpc_bulletin_input_test.py.

--- generalexam/ge_io/wpc_bulletin_input_test.py
+++ generalexam/ge_io/wpc_bulletin_input_test.py
@@ -1,8 +1,8 @@
-"""Unit tests for wpc_bulletin_io.py."""
+"""Unit tests for wpc_bulletin_input.py."""
 
 import unittest
 import numpy
-from generalexam.ge_io import wpc_bulletin_io
+from generalexam.ge_io import wpc_bulletin_input
 
 TOLERANCE = 1e-6
 
@@ -23,8 +23,8 @@ TOP_DIRECTORY_NAME = 'wpc_bulletins'
 BULLETIN_FILE_NAME = 'wpc_bulletins/2018/KWBCCODSUS_HIRES_20180213_0000'
 
 
-class WpcBulletinIoTests(unittest.TestCase):
-    """Each method is a unit test for wpc_bulletin_io.py."""
+class WpcBulletinInputTests(unittest.TestCase):
+    """Each method is a unit test for wpc_bulletin_input.py."""
 
     def test_string_to_latlng_5chars(self):
         """Ensures correct output from _string_to_latlng.
@@ -33,7 +33,7 @@ class WpcBulletinIoTests(unittest.TestCase):
         """
 
         this_latitude_deg, this_longitude_deg = (
-            wpc_bulletin_io._string_to_latlng(LATLNG_STRING_5CHARS))
+            wpc_bulletin_input._string_to_latlng(LATLNG_STRING_5CHARS))
         self.assertTrue(numpy.isclose(
             this_latitude_deg, LATITUDE_FROM_5CHARS_DEG, atol=TOLERANCE))
         self.assertTrue(numpy.isclose(
@@ -46,7 +46,7 @@ class WpcBulletinIoTests(unittest.TestCase):
         """
 
         this_latitude_deg, this_longitude_deg = (
-            wpc_bulletin_io._string_to_latlng(LATLNG_STRING_7CHARS))
+            wpc_bulletin_input._string_to_latlng(LATLNG_STRING_7CHARS))
         self.assertTrue(numpy.isclose(
             this_latitude_deg, LATITUDE_FROM_7CHARS_DEG, atol=TOLERANCE))
         self.assertTrue(numpy.isclose(
@@ -59,7 +59,7 @@ class WpcBulletinIoTests(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            wpc_bulletin_io._string_to_latlng(LATLNG_STRING_5CHARS_BAD)
+            wpc_bulletin_input._string_to_latlng(LATLNG_STRING_5CHARS_BAD)
 
     def test_string_to_latlng_7chars_invalid(self):
         """Ensures correct output from _string_to_latlng.
@@ -68,7 +68,7 @@ class WpcBulletinIoTests(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            wpc_bulletin_io._string_to_latlng(LATLNG_STRING_7CHARS_BAD)
+            wpc_bulletin_input._string_to_latlng(LATLNG_STRING_7CHARS_BAD)
 
     def test_string_to_latlng_8chars(self):
         """Ensures correct output from _string_to_latlng.
@@ -77,12 +77,12 @@ class WpcBulletinIoTests(unittest.TestCase):
         """
 
         with self.assertRaises(ValueError):
-            wpc_bulletin_io._string_to_latlng(LATLNG_STRING_8CHARS)
+            wpc_bulletin_input._string_to_latlng(LATLNG_STRING_8CHARS)
 
     def test_find_file(self):
         """Ensures correct output from find_file."""
 
-        this_file_name = wpc_bulletin_io.find_file(
+        this_file_name = wpc_bulletin_input.find_file(
             valid_time_unix_sec=VALID_TIME_UNIX_SEC,
             top_directory_name=TOP_DIRECTORY_NAME, raise_error_if_missing=False)
         self.assertTrue(this_file_name == BULLETIN_FILE_NAME)
@@ -90,7 +90,7 @@ class WpcBulletinIoTests(unittest.TestCase):
     def test_file_name_to_valid_time(self):
         """Ensures correct output from _file_name_to_valid_time."""
 
-        this_time_unix_sec = wpc_bulletin_io._file_name_to_valid_time(
+        this_time_unix_sec = wpc_bulletin_input._file_name_to_valid_time(
             BULLETIN_FILE_NAME)
         self.assertTrue(this_time_unix_sec == VALID_TIME_UNIX_SEC)
 
