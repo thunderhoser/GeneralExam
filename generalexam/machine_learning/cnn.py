1acb706df0647e743650b3f1ff9ea6ca74b59ba9
thunderhoser <ryan.lagerquist@ou.edu>
2019-03-20 03:01:12 -0500

    Added HACK to cnn.py to deal with bad metafiles.

--- generalexam/machine_learning/cnn.py
+++ generalexam/machine_learning/cnn.py
@@ -248,8 +248,14 @@ def read_metadata(pickle_file_name):
     metadata_dict = pickle.load(pickle_file_handle)
     pickle_file_handle.close()
 
-    if MASK_MATRIX_KEY not in metadata_dict:
+    if MASK_MATRIX_KEY in metadata_dict:
+        try:
+            error_checking.assert_is_numpy_array(metadata_dict[MASK_MATRIX_KEY])
+        except:
+            metadata_dict[MASK_MATRIX_KEY] = metadata_dict[MASK_MATRIX_KEY][0]
+    else:
         metadata_dict.update({MASK_MATRIX_KEY: None})
+
     if NORMALIZATION_TYPE_KEY not in metadata_dict:
         metadata_dict.update({NORMALIZATION_TYPE_KEY: ml_utils.Z_SCORE_STRING})
 
