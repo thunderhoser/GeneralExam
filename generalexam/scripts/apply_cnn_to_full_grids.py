1e79e5f6a290a3fd666a519082d18561211e9417
thunderhoser <ryan.lagerquist@ou.edu>
2019-04-20 23:24:47 -0500

    Added debugging print statement to to apply_cnn_to_full_grids.py.

--- generalexam/scripts/apply_cnn_to_full_grids.py
+++ generalexam/scripts/apply_cnn_to_full_grids.py
@@ -221,10 +221,8 @@ def _run(model_file_name, top_predictor_dir_name, top_gridded_front_dir_name,
             )
             assert grid_name == nwp_model_utils.NAME_OF_EXTENDED_221GRID
 
-            print numpy.sum(mask_matrix)
             mask_matrix[100:-100, 100:-100] = 0
-            print numpy.sum(mask_matrix)
-            print mask_matrix.size
+            print numpy.sum(mask_matrix, axis=1)
 
     num_times = len(valid_times_unix_sec)
     print SEPARATOR_STRING
