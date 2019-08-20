ab76d345bf4354769992e1e8e8ec56f3acec46c6
thunderhoser <ryan.lagerquist@ou.edu>
2019-08-20 13:45:03 -0500

    run_mann_kendall_test.py can now split by season.

--- generalexam/scripts/run_mann_kendall_test.py
+++ generalexam/scripts/run_mann_kendall_test.py
@@ -15,6 +15,7 @@ INPUT_DIR_ARG_NAME = 'input_dir_name'
 FILE_TYPE_ARG_NAME = 'file_type_string'
 FIRST_YEAR_ARG_NAME = 'first_year'
 LAST_YEAR_ARG_NAME = 'last_year'
+SEASON_ARG_NAME = 'season_string'
 CONFIDENCE_LEVEL_ARG_NAME = 'confidence_level'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
@@ -33,6 +34,11 @@ YEAR_HELP_STRING = (
     'This script will use the Mann-Kendall test to assess linear trends for the'
     ' period `{0:s}`...`{1:s}`.'
 ).format(FIRST_YEAR_ARG_NAME, LAST_YEAR_ARG_NAME)
+
+SEASON_HELP_STRING = (
+    'Season.  If you do *not* want to subset by season, leave this empty.  If '
+    'you want to subset by season, must be in the following list:\n{0:s}'
+).format(str(climo_utils.VALID_SEASON_STRINGS))
 
 CONFIDENCE_LEVEL_HELP_STRING = (
     'Confidence level for two-tailed Mann-Kendall test.  At each grid cell, '
@@ -60,6 +66,10 @@ INPUT_ARG_PARSER.add_argument(
     '--' + LAST_YEAR_ARG_NAME, type=int, required=True, help=YEAR_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
+    '--' + SEASON_ARG_NAME, type=str, required=False, default='',
+    help=SEASON_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
     '--' + CONFIDENCE_LEVEL_ARG_NAME, type=float, required=False, default=0.95,
     help=CONFIDENCE_LEVEL_HELP_STRING)
 
@@ -68,66 +78,138 @@ INPUT_ARG_PARSER.add_argument(
     help=OUTPUT_DIR_HELP_STRING)
 
 
-def _read_frequencies(input_file_names):
+def _find_input_files(input_dir_name, file_type_string, first_year, last_year,
+                      season_string):
+    """Finds input files for Mann-Kendall test.
+
+    :param input_dir_name: See documentation at top of file.
+    :param file_type_string: Same.
+    :param first_year: Same.
+    :param last_year: Same.
+    :param season_string: Same.
+    :return: input_file_name_matrix: 2-D numpy array, where
+        input_file_name_matrix[i, j] is the path to the input file for the [i]th
+        year and [j]th month.
+    """
+
+    if season_string in ['', 'None']:
+        desired_months = numpy.linspace(1, 12, num=12, dtype=int)
+    else:
+        desired_months = climo_utils.season_to_months(season_string)
+
+    num_months = len(desired_months)
+
+    error_checking.assert_is_geq(last_year, first_year + 9)
+    num_years = last_year - first_year + 1
+    desired_years = numpy.linspace(
+        first_year, last_year, num=num_years, dtype=int)
+
+    input_file_name_matrix = numpy.full(
+        (num_years, num_months), '', dtype=object
+    )
+
+    for i in range(num_years):
+        for j in range(num_months):
+            this_month_string = '{0:04d}{1:02d}'.format(
+                desired_years[i], desired_months[j]
+            )
+
+            this_month_unix_sec = time_conversion.string_to_unix_sec(
+                this_month_string, '%Y%m')
+
+            this_first_time_unix_sec, this_last_time_unix_sec = (
+                time_conversion.first_and_last_times_in_month(
+                    this_month_unix_sec)
+            )
+
+            this_last_time_unix_sec += 1 - TIME_INTERVAL_SEC
+
+            input_file_name_matrix[i, j] = climo_utils.find_aggregated_file(
+                directory_name=input_dir_name,
+                file_type_string=file_type_string,
+                first_time_unix_sec=this_first_time_unix_sec,
+                last_time_unix_sec=this_last_time_unix_sec,
+                hours=None, months=None, raise_error_if_missing=True)
+
+    return input_file_name_matrix
+
+
+def _read_frequencies(input_file_name_matrix):
     """Reads front frequencies into two numpy arrays.
 
     T = number of years
     M = number of rows in grid
     N = number of columns in grid
 
-    :param input_file_names: length-T list of paths to input files (will be read
-        by `climatology_utils.read_gridded_counts`).
+    :param input_file_name_matrix: See output doc for `_find_input_files`.
     :return: wf_frequency_matrix: T-by-M-by-N numpy array.
         wf_frequency_matrix[t, i, j] is the frequency of warm fronts (fraction
         of time steps with warm fronts) at grid cell [i, j] in the [t]th year.
     :return: cf_frequency_matrix: Same but for cold fronts.
     """
 
-    num_years = len(input_file_names)
+    num_years = input_file_name_matrix.shape[0]
+    num_months = input_file_name_matrix.shape[1]
+
     wf_frequency_matrix = None
     cf_frequency_matrix = None
 
     for i in range(num_years):
-        print('Reading data from: "{0:s}"...'.format(input_file_names[i]))
-        this_count_dict = climo_utils.read_gridded_counts(input_file_names[i])
+        num_times_in_year = 0
 
-        this_num_time_steps = 1 + int(numpy.round(
-            float(
-                this_count_dict[climo_utils.LAST_TIME_KEY] -
-                this_count_dict[climo_utils.FIRST_TIME_KEY]
-            ) / TIME_INTERVAL_SEC
-        ))
+        for j in range(num_months):
+            print('Reading data from: "{0:s}"...'.format(
+                input_file_name_matrix[i, j]
+            ))
 
-        this_wf_frequency_matrix = (
-            this_count_dict[climo_utils.NUM_WF_LABELS_KEY] / this_num_time_steps
-        )
-        this_cf_frequency_matrix = (
-            this_count_dict[climo_utils.NUM_CF_LABELS_KEY] / this_num_time_steps
-        )
-
-        if wf_frequency_matrix is None:
-            wf_frequency_matrix = numpy.full(
-                (num_years,) + this_wf_frequency_matrix.shape, numpy.nan
-            )
-            cf_frequency_matrix = numpy.full(
-                (num_years,) + this_wf_frequency_matrix.shape, numpy.nan
+            this_count_dict = climo_utils.read_gridded_counts(
+                input_file_name_matrix[i, j]
             )
 
-        wf_frequency_matrix[i, ...] = this_wf_frequency_matrix
-        cf_frequency_matrix[i, ...] = this_cf_frequency_matrix
+            num_times_in_month = 1 + int(numpy.round(
+                float(
+                    this_count_dict[climo_utils.LAST_TIME_KEY] -
+                    this_count_dict[climo_utils.FIRST_TIME_KEY]
+                ) / TIME_INTERVAL_SEC
+            ))
+
+            print('Number of time steps in file = {0:d}'.format(
+                num_times_in_month))
+
+            num_times_in_year += num_times_in_month
+            this_num_wf_matrix = this_count_dict[climo_utils.NUM_WF_LABELS_KEY]
+            this_num_cf_matrix = this_count_dict[climo_utils.NUM_CF_LABELS_KEY]
+
+            if wf_frequency_matrix is None:
+                dimensions = (num_years,) + this_num_wf_matrix.shape
+                wf_frequency_matrix = numpy.full(dimensions, 0.)
+                cf_frequency_matrix = numpy.full(dimensions, 0.)
+
+            wf_frequency_matrix[i, ...] = (
+                wf_frequency_matrix[i, ...] + this_num_wf_matrix
+            )
+            cf_frequency_matrix[i, ...] = (
+                cf_frequency_matrix[i, ...] + this_num_cf_matrix
+            )
+
+        wf_frequency_matrix[i, ...] = (
+            wf_frequency_matrix[i, ...] / num_times_in_year
+        )
+        cf_frequency_matrix[i, ...] = (
+            cf_frequency_matrix[i, ...] / num_times_in_year
+        )
 
     return wf_frequency_matrix, cf_frequency_matrix
 
 
-def _read_statistics(input_file_names):
+def _read_statistics(input_file_name_matrix):
     """Reads front statistics into several numpy arrays.
 
     T = number of years
     M = number of rows in grid
     N = number of columns in grid
 
-    :param input_file_names: length-T list of paths to input files (will be read
-        by `climatology_utils.read_gridded_stats`).
+    :param input_file_name_matrix: See output doc for `_find_input_files`.
     :return: front_statistic_dict: Dictionary with the following keys.
     front_statistic_dict["mean_wf_length_matrix_metres"]: T-by-M-by-N numpy
         array.  wf_length_matrix_metres[t, i, j] is the average length of warm
@@ -140,43 +222,87 @@ def _read_statistics(input_file_names):
         area.
     """
 
-    num_years = len(input_file_names)
-    mean_wf_length_matrix_metres = None
-    mean_cf_length_matrix_metres = None
-    mean_wf_area_matrix_m2 = None
-    mean_cf_area_matrix_m2 = None
+    num_years = input_file_name_matrix.shape[0]
+    num_months = input_file_name_matrix.shape[1]
+
+    num_wf_labels_matrix = None
+    sum_wf_length_matrix_metres = None
+    sum_wf_area_matrix_m2 = None
+    num_cf_labels_matrix = None
+    sum_cf_length_matrix_metres = None
+    sum_cf_area_matrix_m2 = None
 
     for i in range(num_years):
-        print('Reading data from: "{0:s}"...'.format(input_file_names[i]))
-        this_statistic_dict = climo_utils.read_gridded_stats(
-            input_file_names[i]
-        )
+        for j in range(num_months):
+            print('Reading data from: "{0:s}"...'.format(
+                input_file_name_matrix[i, j]
+            ))
 
-        if mean_wf_length_matrix_metres is None:
-            dimensions = (
-                (num_years,) +
-                this_statistic_dict[climo_utils.MEAN_WF_LENGTHS_KEY].shape
+            this_statistic_dict = climo_utils.read_gridded_stats(
+                input_file_name_matrix[i, j]
             )
 
-            mean_wf_length_matrix_metres = numpy.full(dimensions, numpy.nan)
-            mean_cf_length_matrix_metres = numpy.full(dimensions, numpy.nan)
-            mean_wf_area_matrix_m2 = numpy.full(dimensions, numpy.nan)
-            mean_cf_area_matrix_m2 = numpy.full(dimensions, numpy.nan)
+            this_num_wf_matrix = this_statistic_dict[
+                climo_utils.NUM_WF_LABELS_KEY]
+            this_num_cf_matrix = this_statistic_dict[
+                climo_utils.NUM_CF_LABELS_KEY]
 
-        mean_wf_length_matrix_metres[i, ...] = this_statistic_dict[
-            climo_utils.MEAN_WF_LENGTHS_KEY]
-        mean_cf_length_matrix_metres[i, ...] = this_statistic_dict[
-            climo_utils.MEAN_CF_LENGTHS_KEY]
-        mean_wf_area_matrix_m2[i, ...] = this_statistic_dict[
-            climo_utils.MEAN_WF_AREAS_KEY]
-        mean_cf_area_matrix_m2[i, ...] = this_statistic_dict[
-            climo_utils.MEAN_CF_AREAS_KEY]
+            if sum_wf_length_matrix_metres is None:
+                dimensions = (
+                    (num_years,) +
+                    this_statistic_dict[climo_utils.MEAN_WF_LENGTHS_KEY].shape
+                )
+
+                num_wf_labels_matrix = numpy.full(dimensions, 0, dtype=float)
+                sum_wf_length_matrix_metres = numpy.full(dimensions, 0.)
+                sum_wf_area_matrix_m2 = numpy.full(dimensions, 0.)
+                num_cf_labels_matrix = numpy.full(dimensions, 0, dtype=float)
+                sum_cf_length_matrix_metres = numpy.full(dimensions, 0.)
+                sum_cf_area_matrix_m2 = numpy.full(dimensions, 0.)
+
+            num_wf_labels_matrix[i, ...] += this_num_wf_matrix
+            num_cf_labels_matrix[i, ...] += this_num_cf_matrix
+
+            this_sum_matrix = (
+                this_num_wf_matrix *
+                this_statistic_dict[climo_utils.MEAN_WF_LENGTHS_KEY]
+            )
+            this_sum_matrix[numpy.isnan(this_sum_matrix)] = 0
+            sum_wf_length_matrix_metres[i, ...] += this_sum_matrix
+
+            this_sum_matrix = (
+                this_num_wf_matrix *
+                this_statistic_dict[climo_utils.MEAN_WF_AREAS_KEY]
+            )
+            this_sum_matrix[numpy.isnan(this_sum_matrix)] = 0
+            sum_wf_area_matrix_m2[i, ...] += this_sum_matrix
+
+            this_sum_matrix = (
+                this_num_cf_matrix *
+                this_statistic_dict[climo_utils.MEAN_CF_LENGTHS_KEY]
+            )
+            this_sum_matrix[numpy.isnan(this_sum_matrix)] = 0
+            sum_cf_length_matrix_metres[i, ...] += this_sum_matrix
+
+            this_sum_matrix = (
+                this_num_cf_matrix *
+                this_statistic_dict[climo_utils.MEAN_CF_AREAS_KEY]
+            )
+            this_sum_matrix[numpy.isnan(this_sum_matrix)] = 0
+            sum_cf_area_matrix_m2[i, ...] += this_sum_matrix
+
+    num_wf_labels_matrix[num_wf_labels_matrix == 0] = numpy.nan
+    num_cf_labels_matrix[num_cf_labels_matrix == 0] = numpy.nan
 
     return {
-        climo_utils.MEAN_WF_LENGTHS_KEY: mean_wf_length_matrix_metres,
-        climo_utils.MEAN_CF_LENGTHS_KEY: mean_cf_length_matrix_metres,
-        climo_utils.MEAN_WF_AREAS_KEY: mean_wf_area_matrix_m2,
-        climo_utils.MEAN_CF_AREAS_KEY: mean_cf_area_matrix_m2
+        climo_utils.MEAN_WF_LENGTHS_KEY:
+            sum_wf_length_matrix_metres / num_wf_labels_matrix,
+        climo_utils.MEAN_WF_AREAS_KEY:
+            sum_wf_area_matrix_m2 / num_wf_labels_matrix,
+        climo_utils.MEAN_CF_LENGTHS_KEY:
+            sum_cf_length_matrix_metres / num_cf_labels_matrix,
+        climo_utils.MEAN_CF_AREAS_KEY:
+            sum_cf_area_matrix_m2 / num_cf_labels_matrix
     }
 
 
@@ -317,7 +443,7 @@ def _mk_test_one_statistic(statistic_matrix, confidence_level):
     return trend_matrix_year01, significance_matrix
 
 
-def _run(input_dir_name, file_type_string, first_year, last_year,
+def _run(input_dir_name, file_type_string, first_year, last_year, season_string,
          confidence_level, output_dir_name):
     """Runs Mann-Kendall test for gridded front frequencies or statistics.
 
@@ -327,35 +453,23 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
     :param file_type_string: Same.
     :param first_year: Same.
     :param last_year: Same.
+    :param season_string: Same.
     :param confidence_level: Same.
     :param output_dir_name: Same.
     """
 
-    error_checking.assert_is_geq(last_year, first_year + 9)
     error_checking.assert_is_geq(confidence_level, 0.9)
     error_checking.assert_is_less_than(confidence_level, 1.)
 
-    num_years = last_year - first_year + 1
-    years = numpy.linspace(first_year, last_year, num=num_years, dtype=int)
+    input_file_name_matrix = _find_input_files(
+        input_dir_name=input_dir_name, file_type_string=file_type_string,
+        first_year=first_year, last_year=last_year, season_string=season_string)
 
-    input_file_names = [None] * num_years
-
-    for i in range(num_years):
-        this_first_time_unix_sec, this_last_time_unix_sec = (
-            time_conversion.first_and_last_times_in_year(years[i])
-        )
-
-        this_last_time_unix_sec += 1 - TIME_INTERVAL_SEC
-
-        input_file_names[i] = climo_utils.find_aggregated_file(
-            directory_name=input_dir_name, file_type_string=file_type_string,
-            first_time_unix_sec=this_first_time_unix_sec,
-            last_time_unix_sec=this_last_time_unix_sec, hours=None, months=None,
-            raise_error_if_missing=True)
+    input_file_name_list = numpy.ravel(input_file_name_matrix).tolist()
 
     if file_type_string == climo_utils.FRONT_COUNTS_STRING:
         wf_frequency_matrix, cf_frequency_matrix = _read_frequencies(
-            input_file_names)
+            input_file_name_matrix)
         print(SEPARATOR_STRING)
 
         wf_trend_matrix_year01, wf_significance_matrix = _mk_test_frequency(
@@ -375,7 +489,7 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
             trend_matrix_year01=wf_trend_matrix_year01,
             significance_matrix=wf_significance_matrix,
             property_name=climo_utils.WF_FREQ_PROPERTY_NAME,
-            input_file_names=input_file_names,
+            input_file_names=input_file_name_list,
             confidence_level=confidence_level)
 
         cf_trend_matrix_year01, cf_significance_matrix = _mk_test_frequency(
@@ -395,12 +509,12 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
             trend_matrix_year01=cf_trend_matrix_year01,
             significance_matrix=cf_significance_matrix,
             property_name=climo_utils.CF_FREQ_PROPERTY_NAME,
-            input_file_names=input_file_names,
+            input_file_names=input_file_name_list,
             confidence_level=confidence_level)
 
         return
 
-    front_statistic_dict = _read_statistics(input_file_names)
+    front_statistic_dict = _read_statistics(input_file_name_matrix)
     print(SEPARATOR_STRING)
 
     wf_length_matrix_m_y01, wf_length_sig_matrix = _mk_test_one_statistic(
@@ -420,7 +534,7 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
         trend_matrix_year01=wf_length_matrix_m_y01,
         significance_matrix=wf_length_sig_matrix,
         property_name=climo_utils.WF_LENGTH_PROPERTY_NAME,
-        input_file_names=input_file_names,
+        input_file_names=input_file_name_list,
         confidence_level=confidence_level)
 
     wf_area_matrix_m2_y01, wf_area_sig_matrix = _mk_test_one_statistic(
@@ -440,7 +554,7 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
         trend_matrix_year01=wf_area_matrix_m2_y01,
         significance_matrix=wf_area_sig_matrix,
         property_name=climo_utils.WF_AREA_PROPERTY_NAME,
-        input_file_names=input_file_names,
+        input_file_names=input_file_name_list,
         confidence_level=confidence_level)
 
     cf_length_matrix_m_y01, cf_length_sig_matrix = _mk_test_one_statistic(
@@ -460,7 +574,7 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
         trend_matrix_year01=cf_length_matrix_m_y01,
         significance_matrix=cf_length_sig_matrix,
         property_name=climo_utils.CF_LENGTH_PROPERTY_NAME,
-        input_file_names=input_file_names,
+        input_file_names=input_file_name_list,
         confidence_level=confidence_level)
 
     cf_area_matrix_m2_y01, cf_area_sig_matrix = _mk_test_one_statistic(
@@ -480,7 +594,7 @@ def _run(input_dir_name, file_type_string, first_year, last_year,
         trend_matrix_year01=cf_area_matrix_m2_y01,
         significance_matrix=cf_area_sig_matrix,
         property_name=climo_utils.CF_AREA_PROPERTY_NAME,
-        input_file_names=input_file_names,
+        input_file_names=input_file_name_list,
         confidence_level=confidence_level)
 
 
@@ -492,6 +606,7 @@ if __name__ == '__main__':
         file_type_string=getattr(INPUT_ARG_OBJECT, FILE_TYPE_ARG_NAME),
         first_year=getattr(INPUT_ARG_OBJECT, FIRST_YEAR_ARG_NAME),
         last_year=getattr(INPUT_ARG_OBJECT, LAST_YEAR_ARG_NAME),
+        season_string=getattr(INPUT_ARG_OBJECT, SEASON_ARG_NAME),
         confidence_level=getattr(INPUT_ARG_OBJECT, CONFIDENCE_LEVEL_ARG_NAME),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
