0532ea5dbdad84a91cb78ace79592bb97b32c171
thunderhoser <ryan.lagerquist@ou.edu>
2020-01-19 17:40:34 -0600

    plot_mann_kendall_test.py now uses lat-long grid.

--- generalexam/scripts/plot_mann_kendall_test.py
+++ generalexam/scripts/plot_mann_kendall_test.py
@@ -17,6 +17,8 @@ from generalexam.plotting import prediction_plotting
 from generalexam.scripts import plot_gridded_stats
 
 MASK_IF_NUM_LABELS_BELOW = 100
+NUM_ROWS_IN_CNN_PATCH = plot_gridded_stats.NUM_ROWS_IN_CNN_PATCH
+NUM_COLUMNS_IN_CNN_PATCH = plot_gridded_stats.NUM_COLUMNS_IN_CNN_PATCH
 
 TITLE_FONT_SIZE = 16
 FIGURE_RESOLUTION_DPI = 300
@@ -32,36 +34,38 @@ OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 INPUT_FILE_HELP_STRING = (
     'Path to input file.  Will be read by '
-    '`climatology_utils.read_mann_kendall_test`.')
-
+    '`climatology_utils.read_mann_kendall_test`.'
+)
 COLOUR_MAP_HELP_STRING = (
     'Name of colour map for linear trend.  Must be accepted by '
-    '`matplotlib.pyplot.get_cmap`.')
-
+    '`matplotlib.pyplot.get_cmap`.'
+)
 MAX_PERCENTILE_HELP_STRING = (
     'Max value in colour map will be [q]th percentile of absolute linear trend '
     'over all grid cells, where q = `{0:s}`.'
 ).format(MAX_PERCENTILE_ARG_NAME)
 
 OUTPUT_DIR_HELP_STRING = (
-    'Name of output directory.  Figures will be saved here.')
+    'Name of output directory.  Figures will be saved here.'
+)
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + INPUT_FILE_ARG_NAME, type=str, required=True,
-    help=INPUT_FILE_HELP_STRING)
-
+    help=INPUT_FILE_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + COLOUR_MAP_ARG_NAME, type=str, required=False, default='bwr',
-    help=COLOUR_MAP_HELP_STRING)
-
+    help=COLOUR_MAP_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + MAX_PERCENTILE_ARG_NAME, type=float, required=False, default=99.,
-    help=MAX_PERCENTILE_HELP_STRING)
-
+    help=MAX_PERCENTILE_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
-    help=OUTPUT_DIR_HELP_STRING)
+    help=OUTPUT_DIR_HELP_STRING
+)
 
 
 def _plot_trend(
@@ -88,22 +92,23 @@ def _plot_trend(
         Max value in colour scheme.  Minimum value will be -1 * max value.
     """
 
-    basemap_dict = plot_gridded_stats._plot_basemap(trend_matrix_year01)
+    basemap_dict = plot_gridded_stats.plot_basemap(
+        data_matrix=trend_matrix_year01
+    )
+
+    figure_object = basemap_dict[plot_gridded_stats.FIGURE_OBJECT_KEY]
     axes_object = basemap_dict[plot_gridded_stats.AXES_OBJECT_KEY]
     basemap_object = basemap_dict[plot_gridded_stats.BASEMAP_OBJECT_KEY]
-    full_grid_name = basemap_dict[plot_gridded_stats.FULL_GRID_NAME_KEY]
-    full_grid_row_limits = basemap_dict[plot_gridded_stats.FULL_GRID_ROWS_KEY]
-    full_grid_column_limits = basemap_dict[
-        plot_gridded_stats.FULL_GRID_COLUMNS_KEY]
+    latitude_matrix_deg = basemap_dict[plot_gridded_stats.LATITUDES_KEY]
+    longitude_matrix_deg = basemap_dict[plot_gridded_stats.LONGITUDES_KEY]
 
     trend_matrix_to_plot_year01 = trend_matrix_year01[
-        full_grid_row_limits[0]:(full_grid_row_limits[1] + 1),
-        full_grid_column_limits[0]:(full_grid_column_limits[1] + 1)
+        NUM_ROWS_IN_CNN_PATCH:-NUM_ROWS_IN_CNN_PATCH,
+        NUM_COLUMNS_IN_CNN_PATCH:-NUM_COLUMNS_IN_CNN_PATCH
     ]
-
     sig_matrix_to_plot = significance_matrix[
-        full_grid_row_limits[0]:(full_grid_row_limits[1] + 1),
-        full_grid_column_limits[0]:(full_grid_column_limits[1] + 1)
+        NUM_ROWS_IN_CNN_PATCH:-NUM_ROWS_IN_CNN_PATCH,
+        NUM_COLUMNS_IN_CNN_PATCH:-NUM_COLUMNS_IN_CNN_PATCH
     ]
 
     if max_colour_percentile is not None:
@@ -112,41 +117,41 @@ def _plot_trend(
         )
 
     colour_norm_object = pyplot.Normalize(
-        vmin=-max_colour_value, vmax=max_colour_value)
+        vmin=-max_colour_value, vmax=max_colour_value
+    )
 
-    prediction_plotting.plot_gridded_counts(
+    prediction_plotting.plot_counts_on_general_grid(
         count_or_frequency_matrix=trend_matrix_to_plot_year01,
+        latitude_matrix_deg=latitude_matrix_deg,
+        longitude_matrix_deg=longitude_matrix_deg,
         axes_object=axes_object, basemap_object=basemap_object,
         colour_map_object=colour_map_object,
-        colour_norm_object=colour_norm_object, full_grid_name=full_grid_name,
-        first_row_in_full_grid=full_grid_row_limits[0],
-        first_column_in_full_grid=full_grid_column_limits[0]
+        colour_norm_object=colour_norm_object
     )
 
-    significant_rows, significant_columns = numpy.where(sig_matrix_to_plot)
-    significant_y_coords = (
-        (significant_rows + 0.5) / sig_matrix_to_plot.shape[0]
-    )
-    significant_x_coords = (
-        (significant_columns + 0.5) / sig_matrix_to_plot.shape[1]
+    sig_latitudes_deg = latitude_matrix_deg[sig_matrix_to_plot == True]
+    sig_longitudes_deg = longitude_matrix_deg[sig_matrix_to_plot == True]
+    sig_x_coords_metres, sig_y_coords_metres = basemap_object(
+        sig_longitudes_deg, sig_latitudes_deg
     )
 
     axes_object.plot(
-        significant_x_coords, significant_y_coords, linestyle='None',
+        sig_x_coords_metres, sig_y_coords_metres, linestyle='None',
         marker=plot_gridded_stats.SIG_MARKER_TYPE,
         markerfacecolor=plot_gridded_stats.SIG_MARKER_COLOUR,
         markeredgecolor=plot_gridded_stats.SIG_MARKER_COLOUR,
         markersize=plot_gridded_stats.SIG_MARKER_SIZE,
-        markeredgewidth=plot_gridded_stats.SIG_MARKER_EDGE_WIDTH,
-        transform=axes_object.transAxes)
+        markeredgewidth=plot_gridded_stats.SIG_MARKER_EDGE_WIDTH
+    )
 
     colour_bar_object = plotting_utils.plot_colour_bar(
         axes_object_or_matrix=axes_object,
         data_matrix=trend_matrix_to_plot_year01,
         colour_map_object=colour_map_object,
         colour_norm_object=colour_norm_object,
-        orientation_string='horizontal', extend_min=False, extend_max=True,
-        fraction_of_axis_length=0.9)
+        orientation_string='horizontal', padding=0.05,
+        extend_min=False, extend_max=True, fraction_of_axis_length=1.
+    )
 
     tick_values = colour_bar_object.ax.get_xticks()
     colour_bar_object.ax.set_xticks(tick_values)
@@ -159,12 +164,14 @@ def _plot_trend(
         ]
 
     colour_bar_object.ax.set_xticklabels(tick_strings)
-    pyplot.title(title_string, fontsize=TITLE_FONT_SIZE)
+    axes_object.set_title(title_string, fontsize=TITLE_FONT_SIZE)
 
     print('Saving figure to: "{0:s}"...'.format(output_file_name))
-    pyplot.savefig(output_file_name, dpi=FIGURE_RESOLUTION_DPI, pad_inches=0,
-                   bbox_inches='tight')
-    pyplot.close()
+    figure_object.savefig(
+        output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+        pad_inches=0, bbox_inches='tight'
+    )
+    pyplot.close(figure_object)
 
 
 def _run(input_file_name, colour_map_name, max_colour_percentile,
@@ -184,7 +191,8 @@ def _run(input_file_name, colour_map_name, max_colour_percentile,
     colour_map_object = pyplot.get_cmap(colour_map_name)
 
     file_system_utils.mkdir_recursive_if_necessary(
-        directory_name=output_dir_name)
+        directory_name=output_dir_name
+    )
 
     print('Reading data from: "{0:s}"...'.format(input_file_name))
     mann_kendall_dict = climo_utils.read_mann_kendall_test(input_file_name)
@@ -199,16 +207,22 @@ def _run(input_file_name, colour_map_name, max_colour_percentile,
         num_labels_matrix < MASK_IF_NUM_LABELS_BELOW
     ] = numpy.nan
 
-    if property_name in [climo_utils.WF_LENGTH_PROPERTY_NAME,
-                         climo_utils.CF_LENGTH_PROPERTY_NAME]:
+    if property_name in [
+            climo_utils.WF_LENGTH_PROPERTY_NAME,
+            climo_utils.CF_LENGTH_PROPERTY_NAME
+    ]:
         trend_matrix_year01 = trend_matrix_year01 * METRES_TO_KM
 
-    if property_name in [climo_utils.WF_AREA_PROPERTY_NAME,
-                         climo_utils.CF_AREA_PROPERTY_NAME]:
+    if property_name in [
+            climo_utils.WF_AREA_PROPERTY_NAME,
+            climo_utils.CF_AREA_PROPERTY_NAME
+    ]:
         trend_matrix_year01 = trend_matrix_year01 * METRES2_TO_THOUSAND_KM2
 
-    if property_name in [climo_utils.WF_FREQ_PROPERTY_NAME,
-                         climo_utils.CF_FREQ_PROPERTY_NAME]:
+    if property_name in [
+            climo_utils.WF_FREQ_PROPERTY_NAME,
+            climo_utils.CF_FREQ_PROPERTY_NAME
+    ]:
         trend_matrix_year01 = trend_matrix_year01 * FRACTION_TO_PERCENT
 
     if property_name == climo_utils.WF_LENGTH_PROPERTY_NAME:
@@ -225,15 +239,18 @@ def _run(input_file_name, colour_map_name, max_colour_percentile,
         title_string = 'Trend in CF frequency (percent per year)'
 
     output_file_name = '{0:s}/{1:s}_trend.jpg'.format(
-        output_dir_name, property_name)
+        output_dir_name, property_name
+    )
 
     _plot_trend(
         trend_matrix_year01=trend_matrix_year01,
         significance_matrix=mann_kendall_dict[
-            climo_utils.SIGNIFICANCE_MATRIX_KEY],
+            climo_utils.SIGNIFICANCE_MATRIX_KEY
+        ],
         colour_map_object=colour_map_object, title_string=title_string,
         output_file_name=output_file_name,
-        max_colour_percentile=max_colour_percentile)
+        max_colour_percentile=max_colour_percentile
+    )
 
 
 if __name__ == '__main__':
