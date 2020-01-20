d174054eb8005bba55ecedf8f553d9d01179e537
thunderhoser <ryan.lagerquist@ou.edu>
2020-01-19 18:14:25 -0600

    Fixed colour bars in plot_cnn_predictions_full_grid.py.

--- generalexam/scripts/plot_cnn_predictions_full_grid.py
+++ generalexam/scripts/plot_cnn_predictions_full_grid.py
@@ -185,7 +185,8 @@ def _plot_one_time(
                 data_matrix=wf_prob_matrix_to_plot,
                 colour_map_object=this_colour_map_object,
                 colour_norm_object=this_colour_norm_object,
-                orientation_string='horizontal', padding=0.06,
+                orientation_string='horizontal',
+                padding=-0.04 if plot_cf_colour_bar else 0.06,
                 extend_min=True, extend_max=False, fraction_of_axis_length=1.
             )
 
