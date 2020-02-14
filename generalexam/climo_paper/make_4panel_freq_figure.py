ad22d5d5df2ce0f430085bb27f9a3351bfdf5776
thunderhoser <ryan.lagerquist@ou.edu>
2020-02-14 01:05:02 -0600

    Tweaked plotting.

--- generalexam/climo_paper/make_4panel_freq_figure.py
+++ generalexam/climo_paper/make_4panel_freq_figure.py
@@ -30,8 +30,8 @@ SEASON_ABBREV_TO_VERBOSE_DICT = {
 
 MAX_WF_FREQUENCY = 0.08
 MAX_CF_FREQUENCY = 0.14
-WF_COLOUR_MAP_OBJECT = pyplot.get_cmap('plasma')
-CF_COLOUR_MAP_OBJECT = pyplot.get_cmap('plasma')
+WF_COLOUR_MAP_OBJECT = pyplot.get_cmap('viridis')
+CF_COLOUR_MAP_OBJECT = pyplot.get_cmap('viridis')
 BORDER_COLOUR = numpy.full(3, 0.)
 
 TITLE_FONT_SIZE = 30
