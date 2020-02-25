4d18bb2559496242659238e59ecb3bb06075a481
thunderhoser <ryan.lagerquist@ou.edu>
2020-02-24 22:44:19 -0600

    Tweaked colour scheme.

--- generalexam/climo_paper/make_4panel_freq_figure.py
+++ generalexam/climo_paper/make_4panel_freq_figure.py
@@ -30,8 +30,8 @@ SEASON_ABBREV_TO_VERBOSE_DICT = {
 
 MAX_WF_FREQUENCY = 0.08
 MAX_CF_FREQUENCY = 0.14
-WF_COLOUR_MAP_OBJECT = pyplot.get_cmap('viridis')
-CF_COLOUR_MAP_OBJECT = pyplot.get_cmap('viridis')
+WF_COLOUR_MAP_OBJECT = pyplot.get_cmap('twilight')
+CF_COLOUR_MAP_OBJECT = pyplot.get_cmap('twilight')
 BORDER_COLOUR = numpy.full(3, 0.)
 
 TITLE_FONT_SIZE = 30
